c8a3fd2742503ac9af0d2f62fc96d06af1c3ce61
gldu <gldu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-01-10 08:51:54 +0000

    feature 17760 add: Review the use of current libraries.
    git-svn-id: http://talendforge.org/svn/tos/trunk@53721 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.camel.components.localprovider/src/main/java/org/talend/designer/camel/components/localprovider/CamelLocalComponentsProvider.java
+++ org.talend.designer.camel.components.localprovider/src/main/java/org/talend/designer/camel/components/localprovider/CamelLocalComponentsProvider.java
@@ -13,16 +13,28 @@
 package org.talend.designer.camel.components.localprovider;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
 import java.net.URL;
 
+import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
+import org.talend.commons.ui.runtime.exception.ExceptionHandler;
+import org.talend.commons.utils.io.FilesUtils;
+import org.talend.core.i18n.Messages;
 import org.talend.core.model.components.AbstractComponentsProvider;
+import org.talend.core.model.components.IComponentsFactory;
 
 /**
  * DOC guanglong.du class global comment. Detailled comment
  */
 public class CamelLocalComponentsProvider extends AbstractComponentsProvider {
+
+    private static Logger logger = Logger.getLogger(CamelLocalComponentsProvider.class);
 
     public CamelLocalComponentsProvider() {
 
@@ -43,5 +55,56 @@ public class CamelLocalComponentsProvider extends AbstractComponentsProvider {
         } catch (Exception e) {
         }
         return null;
+    }
+
+    public void preComponentsLoad() throws IOException {
+        File installationFolder = getInstallationFolder();
+
+        FilesUtils.createFoldersIfNotExists(installationFolder.getAbsolutePath(), false);
+
+        File externalComponentsLocation = getExternalComponentsLocation();
+        if (externalComponentsLocation != null) {
+            if (externalComponentsLocation.exists()) {
+                try {
+                    FileFilter ff = new FileFilter() {
+
+                        public boolean accept(File pathname) {
+                            if (pathname.getName().equals(".svn")) {
+                                return false;
+                            }
+                            return true;
+                        }
+
+                    };
+                    FilesUtils.copyFolder(externalComponentsLocation, installationFolder, false, ff, null, true, true);
+                } catch (IOException e) {
+                    ExceptionHandler.process(e);
+                }
+            } else {
+                logger.warn(Messages
+                        .getString("AbstractComponentsProvider.folderNotExist", externalComponentsLocation.toString())); //$NON-NLS-1$
+            }
+        }
+    }
+
+    public File getInstallationFolder() throws IOException {
+        Bundle b = Platform.getBundle(IComponentsFactory.CAMEL_COMPONENTS_LOCATION);
+
+        File installationFolder = null;
+        IPath nullPath = new Path(""); //$NON-NLS-1$
+        URL url = FileLocator.find(b, nullPath, null);
+        URL fileUrl = FileLocator.toFileURL(url);
+        File bundleFolder = new File(fileUrl.getPath());
+
+        IPath path = new Path(IComponentsFactory.COMPONENTS_INNER_FOLDER);
+        // path = path.append(ComponentUtilities.getExtFolder(getFolderName()));
+
+        installationFolder = new File(bundleFolder, path.toOSString());
+
+        return installationFolder;
+    }
+
+    public String getComponentsLocation() {
+        return new Path(IComponentsFactory.COMPONENTS_INNER_FOLDER).toString();
     }
 }
