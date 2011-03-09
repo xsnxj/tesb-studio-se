a83003d31a00a204d95af2d48ff42106c47ced03
yljin <yljin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-03-09 06:08:29 +0000

    feature 19323 added: Control the context stop . Stop the context when all exchanges are complete.
    git-svn-id: http://talendforge.org/svn/tos/trunk@56370 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.camel.libraries/resources/java/routines/system/MonitorThread.java
@@ -0,0 +1,102 @@
+package routines.system;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.camel.CamelContext;
+
+/**
+ * Author paulJin create 2011-03-04
+ */
+public class MonitorThread extends Thread {
+
+    int detectForNewExchangeInterval;
+
+    int waitForFirstExchange;
+
+    public int getWaitForFirstExchange() {
+        return waitForFirstExchange;
+    }
+
+    public void setWaitForFirstExchange(int waitForFirstExchange) {
+        this.waitForFirstExchange = waitForFirstExchange;
+    }
+
+    int i = 0;
+
+    Set<String> exchangeId = new HashSet<String>();
+
+    CamelContext context;
+
+    public CamelContext getContext() {
+        return context;
+    }
+
+    public void setContext(CamelContext context) {
+        this.context = context;
+    }
+
+    public Set<String> getExchangeId() {
+        return exchangeId;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                synchronized (this) {
+                    if (exchangeId.size() == 0 && i > 0) {
+
+                        if (waitForNextExchange())
+                            break;
+                        else {
+                            this.notifyAll();
+                            this.wait();
+                        }
+                    } else {
+                        i++;
+                        Thread.sleep(waitForFirstExchange);
+                        if (waitForNextExchange())
+                            break;
+                        else {
+                            this.notifyAll();
+                            this.wait();
+                        }
+                    }
+
+                }
+
+            }
+            context.stop();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private boolean waitForNextExchange() throws InterruptedException {
+        int initDelay = 1;
+        boolean isComplete = true;
+        while (true) {
+            Thread.sleep(initDelay);
+            initDelay = initDelay * 10;
+            if (exchangeId.size() != 0) {
+                isComplete = false;
+                break;
+            } else if (initDelay < detectForNewExchangeInterval) {
+                continue;
+            } else {
+                isComplete = true;
+                break;
+            }
+        }
+        return isComplete;
+    }
+
+    public void setDetectForNewExchangeInterval(int detectForNewExchangeInterval) {
+        this.detectForNewExchangeInterval = detectForNewExchangeInterval;
+    }
+}
