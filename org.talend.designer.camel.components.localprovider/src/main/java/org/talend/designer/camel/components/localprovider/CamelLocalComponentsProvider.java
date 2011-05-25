86f64c48e1484339b3f180ee8eff855f184aecbf
gldu <gldu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-05-25 04:05:30 +0000

    feature 21499 add: Add some changes to support the future Routes type in repository.
    git-svn-id: http://talendforge.org/svn/tos/trunk@61044 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.camel.components.localprovider/src/main/java/org/talend/designer/camel/components/localprovider/CamelLocalComponentsProvider.java
+++ org.talend.designer.camel.components.localprovider/src/main/java/org/talend/designer/camel/components/localprovider/CamelLocalComponentsProvider.java
@@ -25,9 +25,11 @@ import org.eclipse.core.runtime.Platform;
 import org.osgi.framework.Bundle;
 import org.talend.commons.ui.runtime.exception.ExceptionHandler;
 import org.talend.commons.utils.io.FilesUtils;
+import org.talend.core.GlobalServiceRegister;
 import org.talend.core.i18n.Messages;
 import org.talend.core.model.components.AbstractComponentsProvider;
 import org.talend.core.model.components.IComponentsFactory;
+import org.talend.core.ui.branding.IBrandingService;
 
 /**
  * DOC guanglong.du class global comment. Detailled comment
@@ -58,8 +60,12 @@ public class CamelLocalComponentsProvider extends AbstractComponentsProvider {
     }
 
     public void preComponentsLoad() throws IOException {
-        File installationFolder = getInstallationFolder();
-
+        File installationFolder = super.getInstallationFolder();
+        IBrandingService breaningService = (IBrandingService) GlobalServiceRegister.getDefault().getService(
+                IBrandingService.class);
+        if (breaningService.isPoweredOnlyCamel()) {
+            installationFolder = getInstallationFolder();
+        }
         FilesUtils.createFoldersIfNotExists(installationFolder.getAbsolutePath(), false);
 
         File externalComponentsLocation = getExternalComponentsLocation();
@@ -88,23 +94,33 @@ public class CamelLocalComponentsProvider extends AbstractComponentsProvider {
     }
 
     public File getInstallationFolder() throws IOException {
-        Bundle b = Platform.getBundle(IComponentsFactory.CAMEL_COMPONENTS_LOCATION);
-
         File installationFolder = null;
-        IPath nullPath = new Path(""); //$NON-NLS-1$
-        URL url = FileLocator.find(b, nullPath, null);
-        URL fileUrl = FileLocator.toFileURL(url);
-        File bundleFolder = new File(fileUrl.getPath());
+        IBrandingService breaningService = (IBrandingService) GlobalServiceRegister.getDefault().getService(
+                IBrandingService.class);
+        if (breaningService.isPoweredOnlyCamel()) {
+            Bundle b = Platform.getBundle(IComponentsFactory.CAMEL_COMPONENTS_LOCATION);
 
-        IPath path = new Path(IComponentsFactory.COMPONENTS_INNER_FOLDER);
-        // path = path.append(ComponentUtilities.getExtFolder(getFolderName()));
+            IPath nullPath = new Path(""); //$NON-NLS-1$
+            URL url = FileLocator.find(b, nullPath, null);
+            URL fileUrl = FileLocator.toFileURL(url);
+            File bundleFolder = new File(fileUrl.getPath());
 
-        installationFolder = new File(bundleFolder, path.toOSString());
+            IPath path = new Path(IComponentsFactory.COMPONENTS_INNER_FOLDER);
+            // path = path.append(ComponentUtilities.getExtFolder(getFolderName()));
 
+            installationFolder = new File(bundleFolder, path.toOSString());
+        } else {
+            installationFolder = super.getInstallationFolder();
+        }
         return installationFolder;
     }
 
     public String getComponentsLocation() {
-        return new Path(IComponentsFactory.COMPONENTS_INNER_FOLDER).toString();
+        IBrandingService breaningService = (IBrandingService) GlobalServiceRegister.getDefault().getService(
+                IBrandingService.class);
+        if (breaningService.isPoweredOnlyCamel()) {
+            return new Path(IComponentsFactory.COMPONENTS_INNER_FOLDER).toString();
+        }
+        return super.getComponentsLocation();
     }
 }
