35c75140328f39ac8b3f9a64008efebb0e366804
rzubairov <rzubairov@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-09-23 14:22:21 +0000

    Tried to fix the broken build from changes #68712 introducing new field in the service model
    git-svn-id: http://talendforge.org/svn/tos/trunk@68744 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository.services/src/org/talend/repository/services/model/services/util/ServicesSwitch.java
+++ org.talend.repository.services/src/org/talend/repository/services/model/services/util/ServicesSwitch.java
@@ -6,14 +6,12 @@
  */
 package org.talend.repository.services.model.services.util;
 
-import java.util.Map;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 
-import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.util.Switch;
 import org.talend.core.model.metadata.builder.connection.AbstractMetadataObject;
 import org.talend.core.model.metadata.builder.connection.Connection;
 import org.talend.core.model.properties.Item;
@@ -39,7 +37,7 @@ import orgomg.cwm.objectmodel.core.Namespace;
  * @see org.talend.repository.services.model.services.ServicesPackage
  * @generated
  */
-public class ServicesSwitch<T> extends Switch<T> {
+public class ServicesSwitch<T> {
     /**
      * The cached model package
      * <!-- begin-user-doc -->
@@ -61,16 +59,14 @@ public class ServicesSwitch<T> extends Switch<T> {
     }
 
     /**
-     * Checks whether this is a switch for the given package.
+     * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @parameter ePackage the package in question.
-     * @return whether this is a switch for the given package.
+     * @return the first non-null result returned by a <code>caseXXX</code> call.
      * @generated
      */
-    @Override
-    protected boolean isSwitchFor(EPackage ePackage) {
-        return ePackage == modelPackage;
+    public T doSwitch(EObject theEObject) {
+        return doSwitch(theEObject.eClass(), theEObject);
     }
 
     /**
@@ -80,7 +76,26 @@ public class ServicesSwitch<T> extends Switch<T> {
      * @return the first non-null result returned by a <code>caseXXX</code> call.
      * @generated
      */
-    @Override
+    protected T doSwitch(EClass theEClass, EObject theEObject) {
+        if (theEClass.eContainer() == modelPackage) {
+            return doSwitch(theEClass.getClassifierID(), theEObject);
+        }
+        else {
+            List<EClass> eSuperTypes = theEClass.getESuperTypes();
+            return
+                eSuperTypes.isEmpty() ?
+                    defaultCase(theEObject) :
+                    doSwitch(eSuperTypes.get(0), theEObject);
+        }
+    }
+
+    /**
+     * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @return the first non-null result returned by a <code>caseXXX</code> call.
+     * @generated
+     */
     protected T doSwitch(int classifierID, EObject theEObject) {
         switch (classifierID) {
             case ServicesPackage.SERVICE_ITEM: {
@@ -122,11 +137,26 @@ public class ServicesSwitch<T> extends Switch<T> {
                 T result = caseAdditionalInfoMap(additionalInfoMap);
                 if (result == null) result = defaultCase(theEObject);
                 return result;
-            }
+            }            
             default: return defaultCase(theEObject);
         }
     }
 
+    /**
+     * Returns the result of interpreting the object as an instance of '<em>Additional Info Map</em>'.
+     * <!-- begin-user-doc -->
+     * This implementation returns null;
+     * returning a non-null result will terminate the switch.
+     * <!-- end-user-doc -->
+     * @param object the target of the switch.
+     * @return the result of interpreting the object as an instance of '<em>Additional Info Map</em>'.
+     * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+     * @generated
+     */
+    public T caseAdditionalInfoMap(Map.Entry<String, String> object) {
+        return null;
+    }
+    
     /**
      * Returns the result of interpreting the object as an instance of '<em>Service Item</em>'.
      * <!-- begin-user-doc -->
@@ -184,21 +214,6 @@ public class ServicesSwitch<T> extends Switch<T> {
      * @generated
      */
     public T caseServicePort(ServicePort object) {
-        return null;
-    }
-
-    /**
-     * Returns the result of interpreting the object as an instance of '<em>Additional Info Map</em>'.
-     * <!-- begin-user-doc -->
-     * This implementation returns null;
-     * returning a non-null result will terminate the switch.
-     * <!-- end-user-doc -->
-     * @param object the target of the switch.
-     * @return the result of interpreting the object as an instance of '<em>Additional Info Map</em>'.
-     * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
-     * @generated
-     */
-    public T caseAdditionalInfoMap(Map.Entry<String, String> object) {
         return null;
     }
 
@@ -363,7 +378,6 @@ public class ServicesSwitch<T> extends Switch<T> {
      * @see #doSwitch(org.eclipse.emf.ecore.EObject)
      * @generated
      */
-    @Override
     public T defaultCase(EObject object) {
         return null;
     }
