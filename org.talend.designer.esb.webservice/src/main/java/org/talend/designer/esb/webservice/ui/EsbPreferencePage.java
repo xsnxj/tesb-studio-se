295ee8087aee9990ae3a5a85bf9f2081e50c02b7
rzubairov <rzubairov@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-10-14 08:18:32 +0000

    tESB* components automated commit after build # 263 of ESB_Tooling_SE
    git-svn-id: http://talendforge.org/svn/tos/trunk@70117 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.esb.webservice/src/main/java/org/talend/designer/esb/webservice/ui/EsbPreferencePage.java
+++ org.talend.designer.esb.webservice/src/main/java/org/talend/designer/esb/webservice/ui/EsbPreferencePage.java
@@ -9,31 +9,27 @@ import org.talend.designer.esb.webservice.i18n.Messages;
 
 public class EsbPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
-    public EsbPreferencePage() {
-        super(GRID);
-    }
+	public EsbPreferencePage() {
+		super(GRID);
+	}
 
-    public void init(IWorkbench arg0) {
-        setPreferenceStore(WebServiceComponentPlugin.getDefault().getPreferenceStore());
-        // setDescription(Messages.getString("esb.preferences.webservice.port.default"));
-    }
+	public void init(IWorkbench arg0) {
+		setPreferenceStore(WebServiceComponentPlugin.getDefault().getPreferenceStore());
+		//setDescription(Messages.getString("esb.preferences.webservice.port.default"));
+	}
 
-    @Override
-    protected void createFieldEditors() {
+	@Override
+	protected void createFieldEditors() {
 
-        IntegerFieldEditor localWebServiceHttpPort = new IntegerFieldEditor(WebServiceComponentPlugin.WS_HTTP_PORT_PREFERENCE,
-                Messages.getString("esb.preferences.webservice.port.default"), getFieldEditorParent());
-        // localWebServiceHttpPort.setValidRange(0, 65535);
-        localWebServiceHttpPort.setValidRange(1024, 65535);
+		IntegerFieldEditor localWebServiceHttpPort = new IntegerFieldEditor(
+				WebServiceComponentPlugin.WS_HTTP_PORT_PREFERENCE,
+				Messages.getString("esb.preferences.webservice.port.default"),
+				getFieldEditorParent());
+		// localWebServiceHttpPort.setValidRange(0, 65535);
+		localWebServiceHttpPort.setValidRange(1024, 65535);
 
-        addField(localWebServiceHttpPort);
-    }
+		addField(localWebServiceHttpPort);
+	}
 
-    @Override
-    public boolean performOk() {
-        boolean result = super.performOk();
-        WebServiceComponentPlugin.getDefault().loadCustomProperty();
-        return result;
-    }
 
 }
