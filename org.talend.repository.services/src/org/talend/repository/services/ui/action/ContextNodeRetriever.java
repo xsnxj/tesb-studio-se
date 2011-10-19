3b04571f8349dd481af10e8ba5ebcc514e0bb089
gliu <gliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-10-19 09:25:47 +0000

    [TESB-3525] Fixed: Add context names to the OSGi feature configuration
    git-svn-id: http://talendforge.org/svn/tos/trunk@70478 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository.services/src/org/talend/repository/services/ui/action/ContextNodeRetriever.java
+++ org.talend.repository.services/src/org/talend/repository/services/ui/action/ContextNodeRetriever.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import java.util.Set;
 
 import org.eclipse.emf.common.util.EList;
+import org.talend.core.model.properties.Item;
 import org.talend.core.model.properties.ProcessItem;
 import org.talend.designer.core.model.utils.emf.talendfile.ContextType;
 import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
@@ -20,44 +21,64 @@ public class ContextNodeRetriever {
 	public static String[] getAllContext(RepositoryNode node) {
 		Set<String> contexts = new HashSet<String>();
 		try {
-			ServiceItem serviceItem = (ServiceItem) node.getObject()
-					.getProperty().getItem();
-			ServiceConnection connection = (ServiceConnection) serviceItem
-					.getConnection();
-			EList<ServicePort> servicePort = connection.getServicePort();
-			for (ServicePort sp : servicePort) {
-				EList<ServiceOperation> serviceOperation = sp
-						.getServiceOperation();
-				for (ServiceOperation so : serviceOperation) {
-					String jobId = so.getReferenceJobId();
-					if (jobId == null) {
-						continue;
-					}
-					RepositoryNode jobNode = RepositoryNodeUtilities
-							.getRepositoryNode(jobId, false);
-					if (jobNode == null) {
-						continue;
-					}
-					ProcessItem processItem = (ProcessItem) jobNode.getObject()
-							.getProperty().getItem();
-					ProcessType process = processItem.getProcess();
-					if (process == null) {
-						continue;
-					}
-					EList context = process.getContext();
-					if (context == null) {
-						continue;
-					}
-					Iterator iterator = context.iterator();
-					while (iterator.hasNext()) {
-						Object next = iterator.next();
-						if (!(next instanceof ContextType))
+			Item item = node.getObject().getProperty().getItem();
+			if (item instanceof ServiceItem) {
+				ServiceItem serviceItem = (ServiceItem) node.getObject()
+						.getProperty().getItem();
+				ServiceConnection connection = (ServiceConnection) serviceItem
+						.getConnection();
+				EList<ServicePort> servicePort = connection.getServicePort();
+				for (ServicePort sp : servicePort) {
+					EList<ServiceOperation> serviceOperation = sp
+							.getServiceOperation();
+					for (ServiceOperation so : serviceOperation) {
+						String jobId = so.getReferenceJobId();
+						if (jobId == null) {
 							continue;
-						ContextType ct = (ContextType) next;
-						String name = ct.getName();
-						contexts.add(name);
+						}
+						RepositoryNode jobNode = RepositoryNodeUtilities
+								.getRepositoryNode(jobId, false);
+						if (jobNode == null) {
+							continue;
+						}
+						ProcessItem processItem = (ProcessItem) jobNode
+								.getObject().getProperty().getItem();
+						ProcessType process = processItem.getProcess();
+						if (process == null) {
+							continue;
+						}
+						EList context = process.getContext();
+						if (context == null) {
+							continue;
+						}
+						Iterator iterator = context.iterator();
+						while (iterator.hasNext()) {
+							Object next = iterator.next();
+							if (!(next instanceof ContextType))
+								continue;
+							ContextType ct = (ContextType) next;
+							String name = ct.getName();
+							contexts.add(name);
+						}
 					}
 				}
+			} else if (item instanceof ProcessItem) {
+				ProcessType process = ((ProcessItem) item).getProcess();
+				if (process != null) {
+					EList context = process.getContext();
+					if (context != null) {
+						Iterator iterator = context.iterator();
+						while (iterator.hasNext()) {
+							Object next = iterator.next();
+							if (!(next instanceof ContextType))
+								continue;
+							ContextType ct = (ContextType) next;
+							String name = ct.getName();
+							contexts.add(name);
+						}
+					}
+				}
+
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
