5c8a73b64f3bd89189fd79ba47488dc172fdeef5
atelesh <atelesh@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-11-04 10:22:59 +0000

    data services: import/create WSDL re-factoring
    git-svn-id: http://talendforge.org/svn/tos/trunk@71569 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository.services/src/org/talend/repository/services/ui/OpenWSDLPage.java
+++ org.talend.repository.services/src/org/talend/repository/services/ui/OpenWSDLPage.java
@@ -16,7 +16,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.HashMap;
@@ -50,19 +49,15 @@ import org.talend.commons.exception.PersistenceException;
 import org.talend.commons.exception.SystemException;
 import org.talend.commons.ui.runtime.exception.ExceptionHandler;
 import org.talend.commons.ui.swt.formtools.LabelledFileField;
-import org.talend.core.GlobalServiceRegister;
-import org.talend.core.IESBService;
 import org.talend.core.model.properties.ByteArray;
 import org.talend.core.model.properties.PropertiesFactory;
 import org.talend.core.model.properties.ReferenceFileItem;
 import org.talend.core.model.repository.ERepositoryObjectType;
-import org.talend.core.model.repository.RepositoryManager;
 import org.talend.core.model.repository.RepositoryViewObject;
 import org.talend.core.repository.model.ProxyRepositoryFactory;
 import org.talend.core.repository.model.ResourceModelUtils;
 import org.talend.repository.ProjectManager;
 import org.talend.repository.model.IProxyRepositoryFactory;
-import org.talend.repository.model.RepositoryNodeUtilities;
 import org.talend.repository.model.IRepositoryNode.ENodeType;
 import org.talend.repository.model.IRepositoryNode.EProperties;
 import org.talend.repository.model.RepositoryNode;
@@ -72,7 +67,6 @@ import org.talend.repository.services.model.services.ServiceItem;
 import org.talend.repository.services.model.services.ServiceOperation;
 import org.talend.repository.services.model.services.ServicePort;
 import org.talend.repository.services.model.services.ServicesFactory;
-import org.talend.repository.services.utils.ESBRepositoryNodeType;
 import org.talend.repository.services.utils.TemplateProcessor;
 
 /**
@@ -114,7 +108,6 @@ public class OpenWSDLPage extends WizardPage {
         check.setSelection(false);
         createWSDL = false;
         check.addSelectionListener(new SelectionAdapter() {
-
             public void widgetSelected(SelectionEvent e) {
                 if (check.getSelection()) {
                     wsdlText.setVisible(false);
@@ -124,58 +117,44 @@ public class OpenWSDLPage extends WizardPage {
                     wsdlText.setVisible(true);
                     createWSDL = false;
                     path = wsdlText.getText();
-                    if (path.trim().length() > 0) {
-                        setPageComplete(true);
-                    } else {
-                        setPageComplete(false);
-                    }
+                    setPageComplete(!path.trim().isEmpty());
                 }
             }
-
         });
+
         GridData gridData = new GridData(SWT.FILL, SWT.CENTER, true, false);
         gridData.horizontalSpan = 5;
         gridData.verticalSpan = 1;
         check.setLayoutData(gridData);
 
-        String[] xmlExtensions = { "*.xml;*.xsd;*.wsdl", "*.*", "*" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-        wsdlText = new LabelledFileField(parentArea, "WSDL", //$NON-NLS-1$
-                xmlExtensions);
-        if (item.getConnection() != null) {
-            wsdlText.setText(((ServiceConnection) item.getConnection()).getWSDLPath());
-        }
-        path = wsdlText.getText();
+        String[] xmlExtensions = { "*.xml;*.xsd;*.wsdl", "*.*", "*" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        wsdlText = new LabelledFileField(parentArea, "WSDL", xmlExtensions); //$NON-NLS-1$
 
-        if (path.trim().length() > 0) {
-            setPageComplete(true);
-        } else {
-            setPageComplete(false);
+        if (null != item.getConnection()) {
+            path = ((ServiceConnection) item.getConnection()).getWSDLPath();
+            wsdlText.setText(path);
         }
+        setPageComplete(!path.trim().isEmpty());
+
         addListener();
         setControl(parentArea);
-
     }
 
     private void addListener() {
         wsdlText.addModifyListener(new ModifyListener() {
-
             public void modifyText(ModifyEvent e) {
                 path = wsdlText.getText();
-                if (path.trim().length() > 0) {
-                    setPageComplete(true);
-                } else {
-                    setPageComplete(false);
-                }
+                setPageComplete(!path.trim().isEmpty());
             }
-
         });
     }
 
+    @SuppressWarnings("unchecked")
     public boolean finish() {
 
         String label = item.getProperty().getLabel();
         String version = item.getProperty().getVersion();
-        String wsdlFileName = label + "_" + version + ".wsdl";
+        String wsdlFileName = label + "_" + version + ".wsdl"; //$NON-NLS-1$ //$NON-NLS-2$
 
         IProxyRepositoryFactory factory = ProxyRepositoryFactory.getInstance();
         if (creation) {
@@ -187,8 +166,9 @@ public class OpenWSDLPage extends WizardPage {
                 ExceptionHandler.process(e);
             }
             repositoryNode = new RepositoryNode(new RepositoryViewObject(item.getProperty()),
-            		repositoryNode.getParent(), ENodeType.REPOSITORY_ELEMENT);
+                    repositoryNode.getParent(), ENodeType.REPOSITORY_ELEMENT);
         }
+
 
         IProject currentProject;
         try {
@@ -204,98 +184,56 @@ public class OpenWSDLPage extends WizardPage {
         }
         IFile fileTemp = currentProject.getFolder("services" + folder).getFile(wsdlFileName);
 
-        if (!createWSDL) {
-            if (path != null && !path.trim().equals("")) {
-                item.setConnection(ServicesFactory.eINSTANCE.createServiceConnection());
-                ((ServiceConnection) item.getConnection()).setWSDLPath(path);
-                File file = new File(path);
-                FileInputStream source;
-                ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                try {
-                    source = new FileInputStream(file);
-                    try {
+        try {
+            item.setConnection(ServicesFactory.eINSTANCE.createServiceConnection());
+            ((ServiceConnection) item.getConnection()).setWSDLPath((createWSDL) ? "" : path);
+            ((ServiceConnection) item.getConnection()).getServicePort().clear();
 
-                        byte[] buf = new byte[1024];
-                        int i = 0;
-                        while ((i = source.read(buf)) != -1) {
-                            bos.write(buf, 0, i);
-                        }
-                    } catch (IOException e) {
-                        ExceptionHandler.process(e);
-                    } finally {
-                        try {
-                            source.close();
-                        } catch (Exception e) {
-                        }
-                        try {
-                            bos.close();
-                        } catch (Exception e) {
-                        }
-                    }
-                } catch (FileNotFoundException e2) {
-                    ExceptionHandler.process(e2);
-                } // copy file to item
-                try {
-                    ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bos.toByteArray());
-                    if (!fileTemp.exists()) {
-                        fileTemp.create(byteArrayInputStream, true, null);
-                    } else {
-                        fileTemp.setContents(byteArrayInputStream, 0, null);
-                    }
-                } catch (CoreException e) {
-                    ExceptionHandler.process(e);
-                }
-                //
-                if (item.getReferenceResources().size() == 0) {
-                    ReferenceFileItem createReferenceFileItem = PropertiesFactory.eINSTANCE.createReferenceFileItem();
-                    ByteArray byteArray = PropertiesFactory.eINSTANCE.createByteArray();
-                    createReferenceFileItem.setContent(byteArray);
-                    createReferenceFileItem.setExtension("wsdl");
-                    item.getReferenceResources().add(createReferenceFileItem);
-                    createReferenceFileItem.getContent().setInnerContent(bos.toByteArray());
-                } else {
-                    ((ReferenceFileItem) item.getReferenceResources().get(0)).getContent().setInnerContent(bos.toByteArray());
-                }
-                //
-                populateModelFromWsdl(factory, path, item, repositoryNode);
-            }
-        } else { // create new wsdl file
-            try {
-                ((ServiceConnection) item.getConnection()).setWSDLPath("");
-                ((ServiceConnection) item.getConnection()).getServicePort().clear();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
+            if (createWSDL) {
+                // create new WSDL file from template
                 Map<String, Object> wsdlInfo = new HashMap<String, Object>();
                 wsdlInfo.put("serviceName", label); //$NON-NLS-1$
 
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
                 String templatePath = "/resources/wsdl-template.wsdl"; //$NON-NLS-1$
                 TemplateProcessor.processTemplate(templatePath, wsdlInfo, new OutputStreamWriter(baos));
-
-                // ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(new byte[0]);
-                ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(baos.toByteArray());
-                if (!fileTemp.exists()) {
-                    fileTemp.create(byteArrayInputStream, true, null);
-                } else {
-                    fileTemp.setContents(byteArrayInputStream, 0, null);
-                }
-            } catch (SystemException e1) {
-                ExceptionHandler.process(e1);
-            } catch (CoreException e) {
-                ExceptionHandler.process(e);
+            } else {
+                // copy WSDL file
+                readWsdlFile(new File(path), baos);
             }
+
+            // store WSDL in service
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(baos.toByteArray());
+            if (!fileTemp.exists()) {
+                fileTemp.create(byteArrayInputStream, true, null);
+            } else {
+                fileTemp.setContents(byteArrayInputStream, 0, null);
+            }
+
+
             //
-            if (item.getReferenceResources().size() == 0) {
-                ReferenceFileItem createReferenceFileItem = PropertiesFactory.eINSTANCE.createReferenceFileItem();
+            ReferenceFileItem createReferenceFileItem = null;
+            if (item.getReferenceResources().isEmpty()) {
+                createReferenceFileItem = PropertiesFactory.eINSTANCE.createReferenceFileItem();
                 ByteArray byteArray = PropertiesFactory.eINSTANCE.createByteArray();
                 createReferenceFileItem.setContent(byteArray);
                 createReferenceFileItem.setExtension("wsdl");
                 item.getReferenceResources().add(createReferenceFileItem);
-                createReferenceFileItem.getContent().setInnerContent("".getBytes());
             } else {
-                ((ReferenceFileItem) item.getReferenceResources().get(0)).getContent().setInnerContent("".getBytes());
+                createReferenceFileItem = (ReferenceFileItem) item.getReferenceResources().get(0);
             }
+            createReferenceFileItem.getContent().setInnerContent(baos.toByteArray());
 
+            //
             populateModelFromWsdl(factory, fileTemp.getLocation().toPortableString(), item, repositoryNode);
+
+        } catch (SystemException e) {
+            ExceptionHandler.process(e);
+        } catch (CoreException e) {
+            ExceptionHandler.process(e);
+        } catch (IOException e) {
+            ExceptionHandler.process(e);
         }
 
         try {
@@ -304,7 +242,6 @@ public class OpenWSDLPage extends WizardPage {
             OpenWSDLEditorAction action = new OpenWSDLEditorAction();
             action.setRepositoryNode(repositoryNode);
             action.run();
-
             return true;
         } catch (PersistenceException e) {
             e.printStackTrace();
@@ -312,12 +249,30 @@ public class OpenWSDLPage extends WizardPage {
         return false;
     }
 
-    private void populateModelFromWsdl(IProxyRepositoryFactory factory, String wsdlPath, ServiceItem serviceItem,
-            RepositoryNode serviceRepositoryNode) {
-
-        WSDLFactory wsdlFactory;
+    private void readWsdlFile(File file, ByteArrayOutputStream bos) throws IOException {
+        FileInputStream source = null;
         try {
-            wsdlFactory = WSDLFactory.newInstance();
+            source = new FileInputStream(file);
+            byte[] buf = new byte[1024];
+            int i = 0;
+            while ((i = source.read(buf)) != -1) {
+                bos.write(buf, 0, i);
+            }
+        } finally {
+            if (null != source) {
+                try {
+                    source.close();
+                } catch (Exception e) {
+                }
+            }
+        }
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    private void populateModelFromWsdl(IProxyRepositoryFactory factory, String wsdlPath,
+            ServiceItem serviceItem, RepositoryNode serviceRepositoryNode) throws SystemException {
+        try {
+            WSDLFactory wsdlFactory = WSDLFactory.newInstance();
             WSDLReader newWSDLReader = wsdlFactory.newWSDLReader();
             newWSDLReader.setExtensionRegistry(wsdlFactory.newPopulatedExtensionRegistry());
             newWSDLReader.setFeature(com.ibm.wsdl.Constants.FEATURE_VERBOSE, false);
@@ -350,9 +305,8 @@ public class OpenWSDLPage extends WizardPage {
                 ((ServiceConnection) serviceItem.getConnection()).getServicePort().add(port);
             }
         } catch (WSDLException e) {
-            ExceptionHandler.process(e);
+            throw new SystemException(e);
         }
-
     }
 
 }
--- org.talend.repository.services/src/org/talend/repository/services/utils/TemplateProcessor.java
+++ org.talend.repository.services/src/org/talend/repository/services/utils/TemplateProcessor.java
@@ -13,55 +13,40 @@ import org.talend.commons.exception.SystemException;
 
 public class TemplateProcessor {
 
-	public static void processTemplate(String tempalatePath, Map<String, Object> contextParams,
-			Writer outputWriter) throws SystemException {
+    public static void processTemplate(String tempalatePath, Map<String, Object> contextParams,
+            Writer outputWriter) throws SystemException {
 
-		VelocityEngine engine = new VelocityEngine();
-		engine.setProperty("resource.loader", "classpath"); //$NON-NLS-1$ //$NON-NLS-2$
-		engine.setProperty("classpath.resource.loader.class", //$NON-NLS-1$
-				"org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader"); //$NON-NLS-1$
-		engine.init();
+        VelocityEngine engine = new VelocityEngine();
+        engine.setProperty("resource.loader", "classpath"); //$NON-NLS-1$ //$NON-NLS-2$
+        engine.setProperty("classpath.resource.loader.class", //$NON-NLS-1$
+                "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader"); //$NON-NLS-1$
+        engine.init();
 
-		VelocityContext context = new VelocityContext(contextParams);
+        VelocityContext context = new VelocityContext(contextParams);
 
-		try {
-			Template template = engine.getTemplate(tempalatePath);
-			template.merge(context, outputWriter);
-			outputWriter.flush();
-//		} catch (ResourceNotFoundException rnfe) {
-//			// couldn't find the template
-//			throw new org.talend.commons.exception.ResourceNotFoundException(rnfe);
-//		} catch (ParseErrorException pee) {
-//			// syntax error: problem parsing the template
-//			throw new org.talend.commons.exception.SystemException(pee);
-//		} catch (MethodInvocationException mie) {
-//			// something invoked in the template threw an exception
-//			throw new org.talend.commons.exception.SystemException(mie);
-		} catch (VelocityException ve) {
-			// couldn't find the template
-			// org.apache.velocity.exception.ResourceNotFoundException;
-			// syntax error: problem parsing the template
-			// org.apache.velocity.exception.ParseErrorException;
-			// something invoked in the template threw an exception
-			// org.apache.velocity.exception.MethodInvocationException;
-			throw new SystemException(ve);
-		} catch (IOException e) {
-			throw new SystemException(e);
-		}
-	}
-
-//	public static void main(String[] args) throws Throwable {
-//
-////		String template = "E:\\talend\\work\\TOS-TIS\\org.talend.repository.services\\resources\\wsdl-template.wsdl";
-//		String template = "resources/wsdl-template.wsdl";
-//
-//        Map<String, Object> wsdlInfo = new HashMap<String, Object>();
-//        wsdlInfo.put("serviceName", "test"); //$NON-NLS-1$
-//
-//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//		TemplateProcessor.processTemplate(template, wsdlInfo, new OutputStreamWriter(baos));
-//
-//		System.out.println(baos.toString());
-//	}
-
+        try {
+            Template template = engine.getTemplate(tempalatePath);
+            template.merge(context, outputWriter);
+            outputWriter.flush();
+//      } catch (ResourceNotFoundException rnfe) {
+//          // couldn't find the template
+//          throw new org.talend.commons.exception.ResourceNotFoundException(rnfe);
+//      } catch (ParseErrorException pee) {
+//          // syntax error: problem parsing the template
+//          throw new org.talend.commons.exception.SystemException(pee);
+//      } catch (MethodInvocationException mie) {
+//          // something invoked in the template threw an exception
+//          throw new org.talend.commons.exception.SystemException(mie);
+        } catch (VelocityException ve) {
+            // couldn't find the template
+            // org.apache.velocity.exception.ResourceNotFoundException;
+            // syntax error: problem parsing the template
+            // org.apache.velocity.exception.ParseErrorException;
+            // something invoked in the template threw an exception
+            // org.apache.velocity.exception.MethodInvocationException;
+            throw new SystemException(ve);
+        } catch (IOException e) {
+            throw new SystemException(e);
+        }
+    }
 }
