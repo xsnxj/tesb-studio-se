a8e2431413bb6a5b790b25d0e8fce8f15fc8ae8c
xpli <xpli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-02-15 06:52:16 +0000

    TESB-4861 Redesign cJMS to use ConnectionFactory component
    git-svn-id: http://talendforge.org/svn/tos/trunk@78280 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.camel.designer/src/main/java/org/talend/camel/designer/component/JMSExternalComponent.java
@@ -0,0 +1,83 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.camel.designer.component;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.talend.core.model.process.AbstractExternalNode;
+import org.talend.core.model.process.IComponentDocumentation;
+import org.talend.core.model.process.IExternalData;
+import org.talend.designer.core.model.components.EParameterName;
+
+/**
+ * @author
+ * 
+ */
+public class JMSExternalComponent extends AbstractExternalNode {
+
+	private JSMExternalComponentMain main;
+
+	public void initialize() {
+		main = new JSMExternalComponentMain(this);
+
+	}
+
+	public int open(Display display) {
+		this.getElementParameter(EParameterName.UPDATE_COMPONENTS.getName())
+				.setValue(Boolean.TRUE);
+		Dialog dialog = main.createDialog(display.getActiveShell());
+		return dialog.open();
+	}
+
+	public int open(Composite parent) {
+		this.getElementParameter(EParameterName.UPDATE_COMPONENTS.getName())
+				.setValue(Boolean.TRUE);
+
+		return open(parent.getDisplay());
+	}
+
+	public void setExternalData(IExternalData persistentData) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void renameInputConnection(String oldName, String newName) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void renameOutputConnection(String oldName, String newName) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public IComponentDocumentation getComponentDocumentation(
+			String componentName, String tempFolderPath) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public IExternalData getTMapExternalData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void renameMetadataColumnName(String conectionName,
+			String oldColumnName, String newColumnName) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ org.talend.camel.designer/src/main/java/org/talend/camel/designer/component/SetConnectionFactoryCommand.java
@@ -0,0 +1,27 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.camel.designer.component;
+
+import org.eclipse.gef.commands.Command;
+
+/**
+ * @author liXiaopeng
+ * 
+ */
+public class SetConnectionFactoryCommand extends Command {
+
+	@Override
+	public void execute() {
+		super.execute();
+	}
+}
