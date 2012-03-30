13682f5dfe0d19d75a76382ed407a81d169c5fa5
cli <cli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-03-30 07:01:12 +0000

    TDI-20257: remove the dependencies for the  org.talend.repository.
    git-svn-id: http://talendforge.org/svn/tos/trunk@80681 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository.view.esb/src/main/java/org/talend/repository/view/esb/ESBRepositoryViewPlugin.java
+++ org.talend.repository.view.esb/src/main/java/org/talend/repository/view/esb/ESBRepositoryViewPlugin.java
@@ -6,18 +6,18 @@ import org.osgi.framework.BundleContext;
 /**
  * The activator class controls the plug-in life cycle
  */
-public class ESBPlugin extends AbstractUIPlugin {
+public class ESBRepositoryViewPlugin extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.talend.repository.view.esb"; //$NON-NLS-1$
 
 	// The shared instance
-	private static ESBPlugin plugin;
+	private static ESBRepositoryViewPlugin plugin;
 	
 	/**
 	 * The constructor
 	 */
-	public ESBPlugin() {
+	public ESBRepositoryViewPlugin() {
 	}
 
 	/*
@@ -43,7 +43,7 @@ public class ESBPlugin extends AbstractUIPlugin {
 	 *
 	 * @return the shared instance
 	 */
-	public static ESBPlugin getDefault() {
+	public static ESBRepositoryViewPlugin getDefault() {
 		return plugin;
 	}
 
