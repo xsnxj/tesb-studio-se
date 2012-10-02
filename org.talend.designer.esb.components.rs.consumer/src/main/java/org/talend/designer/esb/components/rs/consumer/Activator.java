a3381e96fbfc5f02da1990d1acf74744017b6d64
rzubairov <rzubairov@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-10-02 14:00:58 +0000

    ESB_Tooling_SE autocommit - build #453 amarkevich:  reuse org.apache.cxf.jaxrs.provider.dom4j.DOM4JProvider()
    git-svn-id: http://talendforge.org/svn/tos/trunk@91609 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.esb.components.rs.consumer/src/main/java/org/talend/designer/esb/components/rs/consumer/Activator.java
+++ org.talend.designer.esb.components.rs.consumer/src/main/java/org/talend/designer/esb/components/rs/consumer/Activator.java
@@ -9,13 +9,6 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends AbstractUIPlugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "org.talend.designer.esb.components.rs.provider"; //$NON-NLS-1$
-
-	public static final String REST_URI_PREFERENCE = "restServiceDefaultUri";
-
-	public static final String REST_URI_DEFAULT = "http://127.0.0.1:8090/";
-
 	// The shared instance
 	private static Activator plugin;
 
@@ -42,15 +35,6 @@ public class Activator extends AbstractUIPlugin {
 	 */
 	public static Activator getDefault() {
 		return plugin;
-	}
-
-	public void loadCustomProperty() {
-		String defaultRestUri = getPreferenceStore().getString(REST_URI_PREFERENCE);
-		System.getProperties().put(REST_URI_PREFERENCE, defaultRestUri);
-	}
-
-	protected void initializeDefaultPreferences(IPreferenceStore store) {
-		store.setDefault(REST_URI_PREFERENCE, REST_URI_DEFAULT);
 	}
 
 }
