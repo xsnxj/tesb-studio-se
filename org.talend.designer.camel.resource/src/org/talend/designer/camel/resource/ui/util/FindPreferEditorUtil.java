f283e9fa90cdd2229687a08890c8ef1684853395
gliu <gliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-11-05 05:34:08 +0000

    [TESB-7162] change hardcode to use default editor provided by studio Fix WSDL-file association shall not be hardcoded. Should be done via Eclipse standard mechanisms
    git-svn-id: http://talendforge.org/svn/tos/trunk@93218 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.camel.resource/src/org/talend/designer/camel/resource/ui/util/FindPreferEditorUtil.java
+++ org.talend.designer.camel.resource/src/org/talend/designer/camel/resource/ui/util/FindPreferEditorUtil.java
@@ -1,13 +1,12 @@
 package org.talend.designer.camel.resource.ui.util;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.ui.IEditorDescriptor;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.ide.IDE;
 import org.talend.camel.core.model.camelProperties.RouteResourceItem;
 import org.talend.commons.ui.runtime.exception.MessageBoxExceptionHandler;
 import org.talend.core.repository.model.ProxyRepositoryFactory;
@@ -19,39 +18,9 @@ import org.talend.repository.model.IRepositoryNode;
 
 public class FindPreferEditorUtil {
 
-	private static final String JAVA_EDITOR_ID = "org.eclipse.jdt.ui.CompilationUnitEditor";
-
-	private static final String WSDL_EDITOR_ID = "org.eclipse.wst.wsdl.ui.internal.WSDLEditor";
-
-	private static final String XML_EDITOR_ID = "org.eclipse.wst.xml.ui.internal.tabletree.XMLMultiPageEditorPart";
-
 	private static FindPreferEditorUtil INSTANCE = new FindPreferEditorUtil();
 
-	// private static final String EDITOR_EXTENSION_POINT =
-	// "org.eclipse.ui.editors";
-	// private static final String CONTENTTYPE_EXTENSION_POINT =
-	// "org.eclipse.core.contenttype.contentTypes";
-	//
-	// private IConfigurationElement[] editorExtensions;
-	// private IConfigurationElement[] contentTypeExtensions;
-
-	private Map<String, String> predefinedMap = new HashMap<String, String>();
-
 	private FindPreferEditorUtil() {
-		initialize();
-	}
-
-	private void initialize() {
-		// IExtensionRegistry extensionRegistry =
-		// Platform.getExtensionRegistry();
-		// editorExtensions = extensionRegistry
-		// .getConfigurationElementsFor(EDITOR_EXTENSION_POINT);
-		// contentTypeExtensions = extensionRegistry
-		// .getConfigurationElementsFor(CONTENTTYPE_EXTENSION_POINT);
-
-		predefinedMap.put("xml", XML_EDITOR_ID);
-		predefinedMap.put("wsdl", WSDL_EDITOR_ID);
-		predefinedMap.put("java", JAVA_EDITOR_ID);
 	}
 
 	/**
@@ -61,75 +30,30 @@ public class FindPreferEditorUtil {
 	 * @param fileExtension
 	 * @return
 	 */
-	private String findPreferEditor(String fileExtension) {
+	private String findPreferEditor(RouteResourceInput fileEditorInput) {
 		String editorId = RouteResourceEditor.ID;
-		if (fileExtension == null || fileExtension.trim().equals("")) {
+
+		Object underlingFile = fileEditorInput.getAdapter(IFile.class);
+		if (underlingFile == null) {
 			return editorId;
 		}
-		String found = predefinedMap.get(fileExtension.toLowerCase());
-		if (found != null) {
-			return found;
-		}
 
-		IEditorDescriptor defaultEditor = PlatformUI.getWorkbench()
-				.getEditorRegistry().getDefaultEditor("*." + fileExtension);
-		if (defaultEditor != null)
-			return defaultEditor.getId();
-		return editorId;
-		// for (IConfigurationElement e : editorExtensions) {
-		// String id = e.getAttribute("id");
-		// // only care about eclipse extensions
-		// if (id == null || !id.startsWith("org.eclipse")) {
-		// continue;
-		// }
-		//
-		// String supportedExtensions = e.getAttribute("extensions");
-		//
-		// if (supportedExtensions == null || supportedExtensions.equals("")) {
-		// /*
-		// * if there's no "extensions" attribute defined in Editor
-		// * extension declaration then, search from "contentTypeBinding"
-		// * attribute if exist
-		// */
-		// IConfigurationElement[] bindingContentTypes = e
-		// .getChildren("contentTypeBinding");
-		// if (bindingContentTypes == null
-		// || bindingContentTypes.length == 0
-		// || contentTypeExtensions == null) {
-		// continue;
-		// }
-		//
-		// for (IConfigurationElement binding : bindingContentTypes) {
-		// String contentTypeId = binding
-		// .getAttribute("contentTypeId");
-		// for (IConfigurationElement contentType : contentTypeExtensions) {
-		// if (!"content-type".equals(contentType.getName())) {
-		// continue;
-		// }
-		// if (contentTypeId.endsWith(contentType.getAttribute("id"))
-		// && fileExtension.equals(contentType
-		// .getAttribute("file-extensions"))) {
-		// return id;
-		// }
-		// }
-		// }
-		//
-		// } else {
-		// /*
-		// * search from "extensions" defined in Editor extension
-		// * declaration
-		// */
-		// String[] extensions = supportedExtensions.split(",");
-		// for (String s : extensions) {
-		// if (fileExtension.equals(s.trim())) {
-		// return id;
-		// }
-		// }
-		// }
-		// }
-		// return editorId;
+		IEditorDescriptor ed = null;
+		try {
+			ed = IDE.getEditorDescriptor((IFile) underlingFile, true);
+		} catch (PartInitException e) {
+			return editorId;
+		}
+		if (ed == null) {
+			return editorId;
+		}
+		String id = ed.getId();
+		if (id == null || id.trim() == null) {
+			return editorId;
+		}
+		return id;
 	}
-	
+
 	/**
 	 * Open default editor
 	 * 
@@ -139,7 +63,11 @@ public class FindPreferEditorUtil {
 	 */
 	public static void openDefaultEditor(final IWorkbenchPage page,
 			IRepositoryNode node, RouteResourceItem item) {
-		openEditor(page, node, item, RouteResourceEditor.ID);
+		RouteResourceInput fileEditorInput = RouteResourceInput
+				.createInput(item);
+		fileEditorInput.setRepositoryNode(node);
+
+		openEditor(page, fileEditorInput, item, RouteResourceEditor.ID);
 	}
 
 	/**
@@ -151,9 +79,12 @@ public class FindPreferEditorUtil {
 	 */
 	public static void openEditor(final IWorkbenchPage page,
 			IRepositoryNode node, RouteResourceItem item) {
-		openEditor(page, node, item,
-				FindPreferEditorUtil.INSTANCE.findPreferEditor(item
-						.getBindingExtension()));
+		RouteResourceInput fileEditorInput = RouteResourceInput
+				.createInput(item);
+		fileEditorInput.setRepositoryNode(node);
+
+		openEditor(page, fileEditorInput, item,
+				FindPreferEditorUtil.INSTANCE.findPreferEditor(fileEditorInput));
 	}
 
 	/**
@@ -164,13 +95,9 @@ public class FindPreferEditorUtil {
 	 * @param item
 	 */
 	public static void openEditor(final IWorkbenchPage page,
-			IRepositoryNode node, RouteResourceItem item, String editorId) {
+			RouteResourceInput fileEditorInput, RouteResourceItem item,
+			String editorId) {
 		try {
-
-			RouteResourceInput fileEditorInput = RouteResourceInput
-					.createInput(item);
-
-			fileEditorInput.setRepositoryNode(node);
 
 			IEditorPart editorPart = page.findEditor(fileEditorInput);
 
