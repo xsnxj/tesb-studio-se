e81cbd2ffd98a8dd95c48e8c337749e40a526a7c
gliu <gliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-12-19 07:37:57 +0000

    [TESB-7890] Change the generated code structure to use the spring
    git-svn-id: http://talendforge.org/svn/tos/trunk@95625 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.camel.designer/src/main/java/org/talend/camel/designer/util/CamelSpringUtil.java
+++ org.talend.camel.designer/src/main/java/org/talend/camel/designer/util/CamelSpringUtil.java
@@ -4,29 +4,28 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 import org.talend.camel.core.model.camelProperties.CamelProcessItem;
-import org.talend.core.model.utils.JavaResourcesHelper;
 
 public class CamelSpringUtil {
 
 	private static String TMP_SPRING_CONTENT = null;
-	static {
-		TMP_SPRING_CONTENT = getTmpSpringContent();
-	}
 
 	private CamelSpringUtil() {
 	}
 
 	public static String getDefaultContent(CamelProcessItem item) {
+		if(TMP_SPRING_CONTENT == null){
+			TMP_SPRING_CONTENT = getTmpSpringContent();
+		}
 		try {
 			String defaultContent = TMP_SPRING_CONTENT;
-			String projectFolderName = JavaResourcesHelper
-					.getProjectFolderName(item);
-			String packageName = JavaResourcesHelper.getJobFolderName(item
-					.getProperty().getLabel(), item.getProperty().getVersion());
-			String className = item.getProperty().getLabel();
-			defaultContent = defaultContent.replace("@ROUTEBUILDER_CLASS_ID@",
-					className.toLowerCase()).replace("@ROUTEBUILDER_CLASS_NAME@",
-					projectFolderName + "." + packageName + "." + className);
+//			String projectFolderName = JavaResourcesHelper
+//					.getProjectFolderName(item);
+//			String packageName = JavaResourcesHelper.getJobFolderName(item
+//					.getProperty().getLabel(), item.getProperty().getVersion());
+//			String className = item.getProperty().getLabel();
+//			defaultContent = defaultContent.replace("@ROUTEBUILDER_CLASS_ID@",
+//					className.toLowerCase()).replace("@ROUTEBUILDER_CLASS_NAME@",
+//					projectFolderName + "." + packageName + "." + className);
 			return defaultContent;
 		} catch (Exception e) {
 			e.printStackTrace();
