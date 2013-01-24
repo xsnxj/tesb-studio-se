6edfd42bba5150948a69b86144e1ddfdb0b1aab7
zgao <zgao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-01-24 06:18:49 +0000

    update the action with premise check, and also check whether switch the perspective depends on open result.
    git-svn-id: http://talendforge.org/svn/tos/trunk@97167 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.camel.designer/src/main/java/org/talend/camel/designer/ui/OpenTalendJobRefAction.java
+++ org.talend.camel.designer/src/main/java/org/talend/camel/designer/ui/OpenTalendJobRefAction.java
@@ -1,16 +1,37 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package org.talend.camel.designer.ui;
 
 import java.util.List;
 
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.WorkbenchException;
 import org.talend.designer.core.model.components.EmfComponent;
 import org.talend.designer.core.ui.editor.nodes.Node;
 import org.talend.designer.core.ui.editor.nodes.NodePart;
 
+/**
+ * The Action binding to a Talend Component UI eidotr, 
+ * and only effect to cTalendJob component.
+ * Use to support a simple access point for editor job that linked to a cTalendJob component.
+ */
 public class OpenTalendJobRefAction extends org.talend.designer.core.ui.editor.CustomExternalActions {
 
-    private static final String REQUEST_TYPE_OPEN_NODE_PART = "open";
+    private static final String NODE_PARAM_FROM_REPOSITORY_JOB = "FROM_REPOSITORY_JOB";
+	private static final String NODE_PARAM_SELECTED_JOB_NAME = "SELECTED_JOB_NAME";
+	private static final String REQUEST_TYPE_OPEN_NODE_PART = "open";
     private static final String C_TALEND_JOB_COMPONENT_NAME = "cTalendJob";
     private static final String ORG_TALEND_RCP_INTEGRATION_PERSPECTIVE = "org.talend.rcp.perspective";
 
@@ -22,29 +43,51 @@ public class OpenTalendJobRefAction extends org.talend.designer.core.ui.editor.C
         return 0;
     }
 
+    /* Check if this action enable. Will ensure the cTalendJob was selected and has legal value.
+     * @see org.eclipse.gef.ui.actions.WorkbenchPartAction#calculateEnabled()
+     * 
+     */
     @Override
     protected boolean calculateEnabled() {
         List<?> selectedObjects = getSelectedObjects();
         if (selectedObjects.size() != 1) {
+        	//not select one object.
             return false;
         }
         Object select = selectedObjects.get(0);
         if (!(select instanceof NodePart)) {
+        	//not select a node.
             return false;
         }
 
         NodePart nodePart = (NodePart) select;
         Node node = (Node) nodePart.getModel();
         if (!(node.getComponent() instanceof EmfComponent)) {
+        	//not select a component.
             return false;
         }
         EmfComponent component = (EmfComponent) node.getComponent();
         if (!C_TALEND_JOB_COMPONENT_NAME.equals(component.getName())) {
+        	//not cTalendJob component.
             return false;
+        }
+        Boolean isRepositoryJob=(Boolean) node.getElementParameter(NODE_PARAM_FROM_REPOSITORY_JOB).getValue();
+        if(isRepositoryJob==null||!isRepositoryJob) {
+        	//not from repository job.
+        	return false;
+        }
+        String selectedJobName=(String)node.getElementParameter(NODE_PARAM_SELECTED_JOB_NAME).getValue();
+        if(selectedJobName==null||selectedJobName.equals("")) {
+        	//no reference job been choice.
+        	return false;
         }
         return true;
     }
 
+    /* Will send a "open" request to the selected nodePart, so it will try to open the reference job.
+     * If the "open" command success, then will try to switch the perspective to Integration.
+     * @see org.eclipse.jface.action.Action#run()
+     */
     @Override
     public void run() {
         // open in editor, type and count already checked in calculateEnabled()
@@ -53,8 +96,24 @@ public class OpenTalendJobRefAction extends org.talend.designer.core.ui.editor.C
         NodePart nodePart = (NodePart) select;
         nodePart.performRequest(new org.eclipse.gef.Request(REQUEST_TYPE_OPEN_NODE_PART));
 
-        // switch perspective.
+        IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+        if(activePage==null) {
+        	return;
+        }
+        IEditorPart activeEditor = activePage.getActiveEditor();
+        if(activeEditor==null||activeEditor.getEditorSite()==null) {
+        	return;
+        }
+        
+        Node node = (Node) nodePart.getModel();
+        String selectedJobName=(String)node.getElementParameter(NODE_PARAM_SELECTED_JOB_NAME).getValue();
+        String openJobName=activeEditor.getEditorInput().getName();
+        if(!selectedJobName.equals(openJobName)) {
+        	return;
+        }
+        // open/switch editor success and then  try to switch perspective.
         try {
+        	//if current perspective is ORG_TALEND_RCP_INTEGRATION_PERSPECTIVE, will do nothing in under layer.
             PlatformUI.getWorkbench().showPerspective(ORG_TALEND_RCP_INTEGRATION_PERSPECTIVE, PlatformUI.getWorkbench().getActiveWorkbenchWindow());
         } catch (WorkbenchException e) {
             e.printStackTrace();
