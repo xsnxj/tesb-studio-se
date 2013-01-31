ec91d44d2dce2c36c9985756f269cabaeea3de85
mwang <mwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-01-31 10:11:26 +0000

    For feature: TDI-24405 Support Map/Reduce job in Studio, sub-task: TDI-24489 Implement the "Convert" function between common job and M/R job
    git-svn-id: http://talendforge.org/svn/tos/trunk@97600 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.camel.designer/src/main/java/org/talend/camel/designer/RouteProcessConvertServiceImpl.java
+++ org.talend.camel.designer/src/main/java/org/talend/camel/designer/RouteProcessConvertServiceImpl.java
@@ -1,11 +1,10 @@
 package org.talend.camel.designer;
 
-import org.talend.camel.core.model.camelProperties.CamelProcessItem;
 import org.talend.camel.core.model.camelProperties.impl.CamelProcessItemImpl;
 import org.talend.camel.designer.ui.editor.RouteProcess;
 import org.talend.core.model.process.IProcess;
 import org.talend.core.model.properties.Item;
-import org.talend.core.model.properties.JobletProcessItem;
+import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.designer.core.IProcessConvertService;
 
 public class RouteProcessConvertServiceImpl implements IProcessConvertService {
@@ -22,6 +21,35 @@ public class RouteProcessConvertServiceImpl implements IProcessConvertService {
             return process;
         }
         return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.designer.core.IProcessConvertService#doConvert(org.talend.core.model.properties.Item,
+     * org.talend.core.model.repository.IRepositoryViewObject)
+     */
+    public Item doConvert(Item item, IRepositoryViewObject repViewObject) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.designer.core.IProcessConvertService#isOriginalItemDeleted()
+     */
+    public boolean isOriginalItemDeleted() {
+        return false;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.designer.core.IProcessConvertService#isNewItemCreated()
+     */
+    public boolean isNewItemCreated() {
+        return false;
     }
 
 }
