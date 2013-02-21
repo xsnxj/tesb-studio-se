4ea67707a1574286f62969d3e9dffba81f1f9042
zgao <zgao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-02-21 06:00:18 +0000

    [TESB-8002] VersionPart dialog return "" replace as "0.0.0" version.
    git-svn-id: http://talendforge.org/svn/tos/trunk@98392 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.camel.dependencies.ui/src/org/talend/designer/camel/dependencies/ui/dialog/DependencyVersionPart.java
+++ org.talend.designer.camel.dependencies.ui/src/org/talend/designer/camel/dependencies/ui/dialog/DependencyVersionPart.java
@@ -32,38 +32,42 @@ import org.talend.designer.camel.dependencies.ui.UIActivator;
 
 /**
  * Use to create UI part to set/get dependency version.
- * @author GaoZone (modify from {@link org.eclipse.pde.internal.ui.parts.PluginVersionPart}}
+ * 
+ * @author GaoZone (modify from
+ *         {@link org.eclipse.pde.internal.ui.parts.PluginVersionPart}
  * 
  * 
  */
 public class DependencyVersionPart {
 
-
 	/** The min version text. */
 	private Text fMinVersionText;
-	
+
 	/** The min version bound. */
 	private Combo fMinVersionBound;
-	
+
 	/** The max version text. */
 	private Text fMaxVersionText;
-	
+
 	/** The max version bound. */
 	private Combo fMaxVersionBound;
-	
+
 	/** The version range. */
 	private VersionRange fVersionRange;
-	
+
 	/** The is ranged. */
 	private boolean fIsRanged;
-	
+
 	/** The range allowed. */
 	private boolean fRangeAllowed;
 
 	/**
-	 * Instantiates a new dependency version part.
-	 * Support single version input and version range input.
-	 * @param rangeAllowed the range allowed, true means need to fill both min version and max version.
+	 * Instantiates a new dependency version part. Support single version input
+	 * and version range input.
+	 * 
+	 * @param rangeAllowed
+	 *            the range allowed, true means need to fill both min version
+	 *            and max version.
 	 */
 	public DependencyVersionPart(boolean rangeAllowed) {
 		this.fRangeAllowed = rangeAllowed;
@@ -71,25 +75,32 @@ public class DependencyVersionPart {
 
 	/**
 	 * Creates the version fields.
-	 *
-	 * @param comp the comp
-	 * @param createGroup the create group
-	 * @param editable the editable
+	 * 
+	 * @param comp
+	 *            the comp
+	 * @param createGroup
+	 *            the create group
+	 * @param editable
+	 *            the editable
 	 */
-	public void createVersionFields(Composite comp,boolean createGroup, boolean editable) {
+	public void createVersionFields(Composite comp, boolean createGroup,
+			boolean editable) {
 		if (fRangeAllowed)
 			createRangeField(comp, createGroup, editable);
 		else
 			createSingleField(comp, createGroup, editable);
 		preloadFields();
 	}
-	
+
 	/**
 	 * Creates the range field.
-	 *
-	 * @param parent the parent
-	 * @param createGroup the create group
-	 * @param editable the editable
+	 * 
+	 * @param parent
+	 *            the parent
+	 * @param createGroup
+	 *            the create group
+	 * @param editable
+	 *            the editable
 	 */
 	private void createRangeField(Composite parent, boolean createGroup,
 			boolean editable) {
@@ -99,15 +110,18 @@ public class DependencyVersionPart {
 			parent.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 			parent.setLayout(new GridLayout(3, false));
 		}
-		
-		String[] comboItems = new String[] {Messages.DependencyVersionPart_comboInclusive, Messages.DependencyVersionPart_comboExclusive};
+
+		String[] comboItems = new String[] {
+				Messages.DependencyVersionPart_comboInclusive,
+				Messages.DependencyVersionPart_comboExclusive };
 		Label minlabel = new Label(parent, SWT.NONE);
 		minlabel.setText(Messages.DependencyVersionPart_minimumVersion);
 		fMinVersionText = new Text(parent, SWT.SINGLE | SWT.BORDER);
 		fMinVersionText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		fMinVersionText.setEnabled(editable);
 
-		fMinVersionBound = new Combo(parent, SWT.SINGLE | SWT.BORDER | SWT.READ_ONLY);
+		fMinVersionBound = new Combo(parent, SWT.SINGLE | SWT.BORDER
+				| SWT.READ_ONLY);
 		fMinVersionBound.setEnabled(editable);
 		fMinVersionBound.setItems(comboItems);
 
@@ -117,24 +131,29 @@ public class DependencyVersionPart {
 		fMaxVersionText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		fMaxVersionText.setEnabled(editable);
 
-		fMaxVersionBound = new Combo(parent, SWT.SINGLE | SWT.BORDER | SWT.READ_ONLY);
+		fMaxVersionBound = new Combo(parent, SWT.SINGLE | SWT.BORDER
+				| SWT.READ_ONLY);
 		fMaxVersionBound.setEnabled(editable);
 		fMaxVersionBound.setItems(comboItems);
 	}
-	
+
 	/**
 	 * Creates the single field.
-	 *
-	 * @param parent the parent
-	 * @param createGroup the create group
-	 * @param editable the editable
+	 * 
+	 * @param parent
+	 *            the parent
+	 * @param createGroup
+	 *            the create group
+	 * @param editable
+	 *            the editable
 	 */
 	private void createSingleField(Composite parent, boolean createGroup,
 			boolean editable) {
 		if (createGroup) {
 			parent = new Group(parent, SWT.NONE);
 			((Group) parent).setText(getGroupText());
-			parent.setLayoutData(new GridData(GridData.VERTICAL_ALIGN_BEGINNING | GridData.FILL_HORIZONTAL));
+			parent.setLayoutData(new GridData(GridData.VERTICAL_ALIGN_BEGINNING
+					| GridData.FILL_HORIZONTAL));
 			parent.setLayout(new GridLayout(2, false));
 		}
 		Label label = new Label(parent, SWT.NONE);
@@ -150,27 +169,36 @@ public class DependencyVersionPart {
 	 */
 	private void preloadFields() {
 		if (fRangeAllowed) {
-			fMinVersionText.setText((fVersionRange != null) ? fVersionRange.getMinimum().toString() : ""); //$NON-NLS-1$
-			fMaxVersionText.setText((fVersionRange != null && fVersionRange.getMaximum().getMajor() != Integer.MAX_VALUE) ? fVersionRange.getMaximum().toString() : ""); //$NON-NLS-1$
+			fMinVersionText.setText((fVersionRange != null) ? fVersionRange
+					.getMinimum().toString() : ""); //$NON-NLS-1$
+			fMaxVersionText
+					.setText((fVersionRange != null && fVersionRange
+							.getMaximum().getMajor() != Integer.MAX_VALUE) ? fVersionRange
+							.getMaximum().toString() : ""); //$NON-NLS-1$
 
 			if (fVersionRange != null)
-				fMinVersionBound.select((fVersionRange.getIncludeMinimum()) ? 0 : 1);
+				fMinVersionBound.select((fVersionRange.getIncludeMinimum()) ? 0
+						: 1);
 			else
 				fMinVersionBound.select(0);
 
 			if (fVersionRange != null && getMaxVersion().length() > 0)
-				fMaxVersionBound.select((fVersionRange.getIncludeMaximum()) ? 0 : 1);
+				fMaxVersionBound.select((fVersionRange.getIncludeMaximum()) ? 0
+						: 1);
 			else
 				fMaxVersionBound.select(1);
 		}
-		fMinVersionText.setText((fVersionRange != null) ? fVersionRange.getMinimum().toString() : ""); //$NON-NLS-1$
+		fMinVersionText.setText((fVersionRange != null) ? fVersionRange
+				.getMinimum().toString() : ""); //$NON-NLS-1$
 	}
 
 	/**
 	 * Validate version.
-	 *
-	 * @param text the text
-	 * @param textWidget the text widget
+	 * 
+	 * @param text
+	 *            the text
+	 * @param textWidget
+	 *            the text widget
 	 * @return the i status
 	 */
 	private IStatus validateVersion(String text, Text textWidget) {
@@ -179,7 +207,7 @@ public class DependencyVersionPart {
 		if (VersionValidateUtil.validateVersion(text).getSeverity() != IStatus.OK) {
 			String errorMessage = Messages.DependencyVersionPart_InvalidVersionFormat;
 			return new Status(IStatus.ERROR, UIActivator.PLUGIN_ID,
-					IStatus.ERROR,errorMessage, null);
+					IStatus.ERROR, errorMessage, null);
 		}
 
 		return Status.OK_STATUS;
@@ -187,11 +215,13 @@ public class DependencyVersionPart {
 
 	/**
 	 * Validate version range.
-	 *
+	 * 
 	 * @return the i status
 	 */
-	private IStatus validateVersionRange( ) {
-		if ((!fRangeAllowed && getMinVersion().length() == 0) || (fRangeAllowed && (getMinVersion().length() == 0 || getMaxVersion().length() == 0))) {
+	private IStatus validateVersionRange() {
+		if ((!fRangeAllowed && getMinVersion().length() == 0)
+				|| (fRangeAllowed && (getMinVersion().length() == 0 || getMaxVersion()
+						.length() == 0))) {
 			fIsRanged = false;
 			return Status.OK_STATUS;
 		}
@@ -203,7 +233,7 @@ public class DependencyVersionPart {
 		try {
 			v1 = new Version(getMinVersion());
 		} catch (IllegalArgumentException e) {
-			return new Status(IStatus.ERROR,UIActivator.PLUGIN_ID,  
+			return new Status(IStatus.ERROR, UIActivator.PLUGIN_ID,
 					IStatus.ERROR, errorMessage, null);
 		}
 		if (!fRangeAllowed) // version created fine
@@ -212,22 +242,22 @@ public class DependencyVersionPart {
 		try {
 			v2 = new Version(getMaxVersion());
 		} catch (IllegalArgumentException e) {
-			return new Status(IStatus.ERROR,UIActivator.PLUGIN_ID,  
-					IStatus.ERROR,  errorMessage, null);
+			return new Status(IStatus.ERROR, UIActivator.PLUGIN_ID,
+					IStatus.ERROR, errorMessage, null);
 		}
 		if (v1.compareTo(v2) == 0 || v1.compareTo(v2) < 0) {
 			fIsRanged = v1.compareTo(v2) != 0;
 			return Status.OK_STATUS;
 		}
-		return new Status(IStatus.ERROR, UIActivator.PLUGIN_ID,  
-				IStatus.ERROR, Messages.DependencyVersionDialog_versionRangeError, null);
+		return new Status(IStatus.ERROR, UIActivator.PLUGIN_ID, IStatus.ERROR,
+				Messages.DependencyVersionDialog_versionRangeError, null);
 	}
 
 	/**
 	 * Validate full version range text.
-	 *
+	 * 
 	 * @return an OK status if all versions are valid, otherwise the status's
-	 * message will contain an error message.
+	 *         message will contain an error message.
 	 */
 	public IStatus validateFullVersionRangeText() {
 		IStatus status = validateVersion(getMinVersion(), fMinVersionText);
@@ -240,7 +270,7 @@ public class DependencyVersionPart {
 
 	/**
 	 * Gets the min version.
-	 *
+	 * 
 	 * @return the min version
 	 */
 	private String getMinVersion() {
@@ -249,7 +279,7 @@ public class DependencyVersionPart {
 
 	/**
 	 * Gets the max version.
-	 *
+	 * 
 	 * @return the max version
 	 */
 	private String getMaxVersion() {
@@ -260,7 +290,7 @@ public class DependencyVersionPart {
 
 	/**
 	 * Gets the min inclusive.
-	 *
+	 * 
 	 * @return the min inclusive
 	 */
 	private boolean getMinInclusive() {
@@ -271,7 +301,7 @@ public class DependencyVersionPart {
 
 	/**
 	 * Gets the max inclusive.
-	 *
+	 * 
 	 * @return the max inclusive
 	 */
 	private boolean getMaxInclusive() {
@@ -282,7 +312,7 @@ public class DependencyVersionPart {
 
 	/**
 	 * Extract single version from text.
-	 *
+	 * 
 	 * @return the string
 	 */
 	private String extractSingleVersionFromText() {
@@ -295,13 +325,14 @@ public class DependencyVersionPart {
 
 	/**
 	 * Gets the version.
-	 *
+	 * 
 	 * @return the version
 	 */
 	public String getVersion() {
 		String version;
 		if (fIsRanged) {
-			// if versions are equal they must be inclusive for a range to be valid
+			// if versions are equal they must be inclusive for a range to be
+			// valid
 			// blindly set for the user
 			String minV = getMinVersion();
 			String maxV = getMaxVersion();
@@ -309,38 +340,55 @@ public class DependencyVersionPart {
 			boolean maxI = getMaxInclusive();
 			if (minV.equals(maxV))
 				minI = maxI = true;
-			version = new VersionRange(new Version(minV), minI, new Version(maxV), maxI).toString();
+			VersionRange versionRange = new VersionRange(new Version(minV),
+					minI, new Version(maxV), maxI);
+			if (versionRange.equals(VersionRange.emptyRange)) {
+				version = "";
+			} else {
+				version = versionRange.toString();
+			}
 		} else {
 			String singleversion = extractSingleVersionFromText();
 			if (singleversion == null || singleversion.length() == 0)
-				version = ""; //$NON-NLS-1$
-			else
-				version = new Version(singleversion).toString();
+				version = "";
+			else {
+				Version versionBean = new Version(singleversion);
+				if (versionBean.equals(Version.emptyVersion)) {
+					version = "";
+				} else {
+					version = versionBean.toString();
+				}
+			}
 		}
 		return version;
 	}
 
 	/**
 	 * Adds the listeners.
-	 *
-	 * @param minListener the min listener
-	 * @param selectionListener the selection listener
+	 * 
+	 * @param minListener
+	 *            the min listener
+	 * @param selectionListener
+	 *            the selection listener
 	 */
-	public void addListeners(ModifyListener minListener, SelectionListener selectionListener) {
-		if (fMinVersionText != null && minListener != null)
-		{	fMinVersionText.addModifyListener(minListener);}
-		if (fRangeAllowed && fMaxVersionText != null && minListener != null)
-			{fMaxVersionText.addModifyListener(minListener);}
-		if(fRangeAllowed&&fMinVersionBound!=null) {
+	public void addListeners(ModifyListener minListener,
+			SelectionListener selectionListener) {
+		if (fMinVersionText != null && minListener != null) {
+			fMinVersionText.addModifyListener(minListener);
+		}
+		if (fRangeAllowed && fMaxVersionText != null && minListener != null) {
+			fMaxVersionText.addModifyListener(minListener);
+		}
+		if (fRangeAllowed && fMinVersionBound != null) {
 			fMinVersionBound.addSelectionListener(selectionListener);
 			fMaxVersionBound.addSelectionListener(selectionListener);
 		}
-		
+
 	}
 
 	/**
 	 * Gets the group text.
-	 *
+	 * 
 	 * @return the group text
 	 */
 	protected String getGroupText() {
@@ -349,15 +397,18 @@ public class DependencyVersionPart {
 
 	/**
 	 * Sets the version.
-	 *
-	 * @param version the new version
+	 * 
+	 * @param version
+	 *            the new version
 	 */
 	public void setVersion(String version) {
 		try {
 			if (version != null && !version.equals("")) { //$NON-NLS-1$
 				fVersionRange = new VersionRange(version);
 				Version max = fVersionRange.getMaximum();
-				if (max.getMajor() != Integer.MAX_VALUE && fVersionRange.getMinimum().compareTo(fVersionRange.getMaximum()) < 0)
+				if (max.getMajor() != Integer.MAX_VALUE
+						&& fVersionRange.getMinimum().compareTo(
+								fVersionRange.getMaximum()) < 0)
 					fIsRanged = true;
 			}
 		} catch (IllegalArgumentException e) {
