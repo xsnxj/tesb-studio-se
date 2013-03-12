e700e0f92bf6a4060f1836b665443b62e6d72ada
esb_tooling_build <esb_tooling_build@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-03-12 13:52:23 +0000

    ESB_Tooling_SE autocommit - build #630 Alex Telesh:  improve components & additional javajet providers + add copyright     headers
    git-svn-id: http://talendforge.org/svn/tos/trunk@99445 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.esb.components.ws.consumer/src/main/java/org/talend/esb/tooling/component/provider/ESBComponentsProvider.java
+++ org.talend.designer.esb.components.ws.consumer/src/main/java/org/talend/esb/tooling/component/provider/ESBComponentsProvider.java
@@ -1,6 +1,19 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package org.talend.esb.tooling.component.provider;
 
 import java.io.File;
+import java.net.URL;
 
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
@@ -9,24 +22,21 @@ import org.talend.designer.esb.webservice.WebServiceComponentPlugin;
 
 public class ESBComponentsProvider extends AbstractComponentsProvider {
 
-	private File components;
+    private File providedLocation = null;
 
-	@Override
-	protected File getExternalComponentsLocation() {
-		if (null == components) {
-			try {
-				components = new File(
-					FileLocator.toFileURL(
-						FileLocator.find(
-							WebServiceComponentPlugin.getDefault().getBundle(),
-							new Path("components"),
-							null)).getPath());
-			} catch (Exception e) {
-				WebServiceComponentPlugin.getDefault().getLog().log(
-					WebServiceComponentPlugin.getStatus(null, e));
-			}
-		}
-		return components;
-	}
+    @Override
+    protected File getExternalComponentsLocation() {
+        if (null == providedLocation) {
+            WebServiceComponentPlugin plugin = WebServiceComponentPlugin.getDefault();
+            try {
+                URL url = FileLocator.find(plugin.getBundle(), new Path("components"), null); //$NON-NLS-1$
+                url = FileLocator.toFileURL(url);
+                providedLocation = new File(url.getPath());
+            } catch (Exception e) {
+                plugin.getLog().log(WebServiceComponentPlugin.getStatus(null, e));
+            }
+        }
+        return providedLocation;
+    }
 
 }
--- org.talend.designer.esb.components.ws.consumer/src/main/java/org/talend/esb/tooling/component/provider/ESBJetFileProvider.java
+++ org.talend.designer.esb.components.ws.consumer/src/main/java/org/talend/esb/tooling/component/provider/ESBJetFileProvider.java
@@ -1,9 +1,20 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package org.talend.esb.tooling.component.provider;
 
 import java.io.File;
 import java.net.URL;
 
-import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
 import org.talend.designer.codegen.additionaljet.AbstractJetFileProvider;
@@ -11,18 +22,21 @@ import org.talend.designer.esb.webservice.WebServiceComponentPlugin;
 
 public class ESBJetFileProvider extends AbstractJetFileProvider {
 
-	private static Logger logger = Logger.getLogger(ESBJetFileProvider.class);
+    private File providedLocation = null;
 
-	protected File getExternalFrameLocation() {
-		try {
-			URL localURL1 = FileLocator.find(
-					WebServiceComponentPlugin.getDefault().getBundle(),
-					new Path("additional"), null);
-			URL localURL2 = FileLocator.toFileURL(localURL1);
-			return new File(localURL2.getPath());
-		} catch (Exception localException) {
-			logger.error(localException);
-		}
-		return null;
-	}
+    @Override
+    protected File getExternalFrameLocation() {
+        if (null == providedLocation) {
+            WebServiceComponentPlugin plugin = WebServiceComponentPlugin.getDefault();
+            try {
+                URL url = FileLocator.find(plugin.getBundle(), new Path("additional"), null); //$NON-NLS-1$
+                url = FileLocator.toFileURL(url);
+                providedLocation = new File(url.getPath());
+            } catch (Exception e) {
+                plugin.getLog().log(WebServiceComponentPlugin.getStatus(null, e));
+            }
+        }
+        return providedLocation;
+    }
+
 }
