4cdfbecbb2ab25b614970ccb4bbf8b91d17e2dea
amarkevich <amarkevich@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-05-16 15:28:15 +0000

    TESB-9641 Cannot import a WSDL with complex structure (at customer POC)
    git-svn-id: http://talendforge.org/svn/tos/trunk@102867 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository.services/src/org/talend/repository/services/utils/SchemaUtil.java
+++ org.talend.repository.services/src/org/talend/repository/services/utils/SchemaUtil.java
@@ -14,18 +14,12 @@ package org.talend.repository.services.utils;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Vector;
 
 import javax.wsdl.Definition;
-import javax.wsdl.Import;
 import javax.wsdl.Types;
 import javax.wsdl.extensions.ExtensibilityElement;
 import javax.wsdl.extensions.schema.Schema;
-import javax.wsdl.extensions.schema.SchemaImport;
-import javax.wsdl.extensions.schema.SchemaReference;
 
 import org.apache.ws.commons.schema.XmlSchema;
 import org.apache.ws.commons.schema.XmlSchemaCollection;
@@ -40,16 +34,6 @@ public class SchemaUtil {
     public SchemaUtil(Definition wsdlDefinition) {
         if (null != wsdlDefinition.getTypes()) {
             init(wsdlDefinition.getTypes());
-        } else {
-            @SuppressWarnings("unchecked")
-            Map<String, List<Import>> imports = wsdlDefinition.getImports();
-            for (List<Import> wsdlImports : imports.values()) {
-                for (Import wsdlImport : wsdlImports) {
-                    if (null != wsdlImport.getDefinition().getTypes()) {
-                        init(wsdlImport.getDefinition().getTypes());
-                    }
-                }
-            }
         }
     }
 
@@ -58,11 +42,6 @@ public class SchemaUtil {
         Collection<Schema> schemas = findExtensibilityElement(types.getExtensibilityElements(), Schema.class);
         for (Schema schema : schemas) {
             createXmlSchema(schema.getElement(), schema.getDocumentBaseURI());
-
-            Map importElement = schema.getImports();
-            if (importElement != null && importElement.size() > 0) {
-                findImportSchema(importElement);
-            }
         }
     }
 
@@ -84,64 +63,6 @@ public class SchemaUtil {
         xmlSchemaCollection.read(schemaElement, SCHEMA_SYSTEM_ID);
     }
 
-    private void findImportSchema(Map importElement) {
-        findImportSchema(importElement, new ArrayList<String>());
-    }
-
-    private void findImportSchema(Map importElement,
-            Collection<String> documentBaseList) {
-        Iterator keyIterator = importElement.keySet().iterator();
-        while (keyIterator.hasNext()) {
-            String key = keyIterator.next().toString();
-            Vector importEle = (Vector) importElement.get(key);
-
-            for (int i = 0; i < importEle.size(); i++) {
-                Map recurseImport = null;
-                SchemaImport schemaImport = (SchemaImport) importEle.elementAt(i);
-                boolean isHaveImport = false;
-                if (schemaImport.getReferencedSchema() != null) {
-                    Element refSchema = schemaImport.getReferencedSchema().getElement();
-                    String documentBase = schemaImport.getReferencedSchema().getDocumentBaseURI();
-
-                    if (schemaImport != null) {
-                        if (schemaImport.getReferencedSchema() != null) {
-                            recurseImport = schemaImport.getReferencedSchema().getImports();
-                            if (recurseImport != null && recurseImport.size() > 0 && !documentBaseList.contains(documentBase)) {
-                                isHaveImport = true;
-                                documentBaseList.add(documentBase);
-                            }
-                        }
-                    }
-
-                    createXmlSchema(refSchema, documentBase);
-                }
-
-                if (isHaveImport) {
-                    findImportSchema(recurseImport);
-                }
-
-                if (schemaImport != null) {
-                    if (schemaImport.getReferencedSchema() != null) {
-                        @SuppressWarnings("unchecked")
-                        List<SchemaReference> includeElements = schemaImport.getReferencedSchema().getIncludes();
-                        findIncludesSchema(includeElements);
-                    }
-                }
-
-            }
-        }
-    }
-
-    private void findIncludesSchema(List<SchemaReference> includeElements) {
-        if (includeElements == null || includeElements.size() == 0) {
-            return;
-        }
-        for (SchemaReference schemaReference : includeElements) {
-            Element schemaElement = schemaReference.getReferencedSchema().getElement();
-            String documentBase = schemaReference.getReferencedSchema().getDocumentBaseURI();
-            createXmlSchema(schemaElement, documentBase);
-        }
-    }
 
     public XmlSchema[] getSchemas() {
         return xmlSchemaCollection.getXmlSchema(SCHEMA_SYSTEM_ID);
