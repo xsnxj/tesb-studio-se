fb1b39b0bc67e11b65df1035d39f6c5db79c4526
amarkevich <amarkevich@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-05-31 11:06:44 +0000

    open wsdl using ServiceItem
    git-svn-id: http://talendforge.org/svn/tos/trunk@103623 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository.services/src/org/talend/repository/services/action/OpenWSDLEditorAction.java
+++ org.talend.repository.services/src/org/talend/repository/services/action/OpenWSDLEditorAction.java
@@ -5,10 +5,7 @@ import java.util.Properties;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.jface.viewers.StructuredViewer;
-import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.ui.IPerspectiveDescriptor;
-import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.WorkbenchException;
@@ -16,16 +13,13 @@ import org.eclipse.ui.intro.IIntroSite;
 import org.eclipse.ui.intro.config.IIntroAction;
 import org.talend.commons.exception.ExceptionHandler;
 import org.talend.commons.ui.runtime.image.ImageProvider;
-import org.talend.core.model.repository.ERepositoryObjectType;
+import org.talend.core.model.properties.Item;
 import org.talend.core.model.utils.RepositoryManagerHelper;
-import org.talend.core.repository.model.ProxyRepositoryFactory;
 import org.talend.core.repository.seeker.RepositorySeekerManager;
 import org.talend.core.repository.ui.actions.metadata.AbstractCreateAction;
 import org.talend.core.ui.branding.IBrandingConfiguration;
 import org.talend.designer.core.DesignerPlugin;
 import org.talend.repository.model.IRepositoryNode;
-import org.talend.repository.model.IRepositoryNode.EProperties;
-import org.talend.repository.model.ProjectRepositoryNode;
 import org.talend.repository.model.RepositoryNode;
 import org.talend.repository.services.model.services.ServiceItem;
 import org.talend.repository.services.model.services.util.EServiceCoreImage;
@@ -35,127 +29,62 @@ import org.talend.repository.ui.views.IRepositoryView;
 
 public class OpenWSDLEditorAction extends AbstractCreateAction implements IIntroAction {
 
-    private final static String ID = "org.talend.repository.services.utils.LocalWSDLEditor";
+    private final static String EDITOR_ID = "org.talend.repository.services.utils.LocalWSDLEditor";
+    private final static String PERSPECTIVE_ID = IBrandingConfiguration.PERSPECTIVE_DI_ID;
 
-    private ERepositoryObjectType currentNodeType;
+    private ServiceItem serviceItem;
 
     public OpenWSDLEditorAction() {
         this.setText("Open WSDL Editor");
         this.setToolTipText("Open WSDL Editor");
 
         this.setImageDescriptor(ImageProvider.getImageDesc(EServiceCoreImage.SERVICE_ICON));
-        currentNodeType = ESBRepositoryNodeType.SERVICES;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.talend.core.repository.ui.actions.metadata.AbstractCreateAction#init(org.talend.repository.model.RepositoryNode
-     * )
-     */
     @Override
     protected void init(RepositoryNode node) {
-        ERepositoryObjectType nodeType = (ERepositoryObjectType) node.getProperties(EProperties.CONTENT_TYPE);
-        if (!currentNodeType.equals(nodeType)) {
-            return;
+        // disable context menu action except service node
+        setEnabled(ESBRepositoryNodeType.SERVICES == node.getObjectType() && isLastVersion(node));
+
+        // anyway initialize for double-click
+        if (null != node.getObject()) {
+            Item item = node.getObject().getProperty().getItem();
+            if (item instanceof ServiceItem) {
+                serviceItem = (ServiceItem) item;
+            }
         }
-        ProxyRepositoryFactory.getInstance();
-        switch (node.getType()) {
-        case REPOSITORY_ELEMENT:
-            break;
-        default:
-            return;
-        }
-        boolean flag = true;
-        if (node.getObject() == null) {
-            flag = false;
-        }
-        if (flag) {
-            flag = isLastVersion(node);
-        }
-        setEnabled(flag);
     }
 
-    @SuppressWarnings("rawtypes")
-	@Override
-    public Class getClassForDoubleClick() {
+    @Override
+    public Class<?> getClassForDoubleClick() {
         return ServiceItem.class;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.talend.repository.ui.actions.AContextualAction#doRun()
-     */
     @Override
     protected void doRun() {
-        if (repositoryNode == null) {
-            repositoryNode = getCurrentRepositoryNode();
-        }
-        if (repositoryNode.getObjectType() == ERepositoryObjectType.SERVICESPORT) {
-            repositoryNode = repositoryNode.getParent();
-        }
-        if (isToolbar()) {
-            if (repositoryNode != null && repositoryNode.getContentType() != currentNodeType) {
-                repositoryNode = null;
-            }
-            if (repositoryNode == null) {
-                repositoryNode = getRepositoryNodeForDefault(currentNodeType);
-            }
-        }
-        if (repositoryNode.getObject() == null) {
-            return;
-        }
+        IFile file = WSDLUtils.getWsdlFile(serviceItem);
+        ServiceEditorInput editorInput = new ServiceEditorInput(file, serviceItem);
+        editorInput.setReadOnly(!DesignerPlugin.getDefault().getProxyRepositoryFactory().isEditableAndLockIfPossible(serviceItem));
         try {
-        	ServiceItem serviceItem = (ServiceItem) repositoryNode.getObject().getProperty().getItem();
-        	IFile file = WSDLUtils.getWsdlFile(serviceItem);
-        	ServiceEditorInput editorInput=new ServiceEditorInput(file, serviceItem);
-        	if (DesignerPlugin.getDefault().getProxyRepositoryFactory().isEditableAndLockIfPossible(serviceItem)) {
-        		editorInput.setReadOnly(false);
-        	} else {
-        		editorInput.setReadOnly(true);
-        	}
-        	IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-            page.openEditor(editorInput, ID, true);
+            PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().openEditor(editorInput, EDITOR_ID, true);
         } catch (CoreException e) {
             ExceptionHandler.process(e);
         } 
     }
 
-    public void setRepositoryNode(RepositoryNode repositoryNode) {
-        this.repositoryNode = repositoryNode;
+    public void setServiceItem(ServiceItem serviceItem) {
+        this.serviceItem = serviceItem;
     }
 
     public void run(IIntroSite site, Properties params) {
         PlatformUI.getWorkbench().getIntroManager().closeIntro(PlatformUI.getWorkbench().getIntroManager().getIntro());
 
-        do_SwitchPerspective_ExpandRepositoryNode_SelectNodeItem(IBrandingConfiguration.PERSPECTIVE_DI_ID,
-                ESBRepositoryNodeType.SERVICES, params.getProperty("nodeId"));
-
-        repositoryNode = (RepositoryNode) RepositorySeekerManager.getInstance().searchRepoViewNode(params.getProperty("nodeId"), false);
-
-        doRun();
-    }
-
-    private void do_SwitchPerspective_ExpandRepositoryNode_SelectNodeItem(String perspectiveId,
-            ERepositoryObjectType repositoryNodeType, String nodeItemId) {
-
-        IWorkbenchWindow workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-        if (null == workbenchWindow) {
-            return;
-        }
-        IWorkbenchPage workbenchPage = workbenchWindow.getActivePage();
-        if (null == workbenchPage) {
-            return;
-        }
-
-        IPerspectiveDescriptor currentPerspective = workbenchPage.getPerspective();
-        if (!perspectiveId.equals(currentPerspective.getId())) {
+        IPerspectiveDescriptor currentPerspective = site.getPage().getPerspective();
+        if (!PERSPECTIVE_ID.equals(currentPerspective.getId())) {
             // show required perspective
+            IWorkbenchWindow workbenchWindow = site.getWorkbenchWindow();
             try {
-                workbenchWindow.getWorkbench().showPerspective(perspectiveId, workbenchWindow);
-                workbenchPage = workbenchWindow.getActivePage();
+                workbenchWindow.getWorkbench().showPerspective(PERSPECTIVE_ID, workbenchWindow);
             } catch (WorkbenchException e) {
                 ExceptionHandler.process(e);
                 return;
@@ -164,25 +93,18 @@ public class OpenWSDLEditorAction extends AbstractCreateAction implements IIntro
 
         // find repository node
         IRepositoryView view = RepositoryManagerHelper.getRepositoryView();
-        RepositoryNode repositoryNode = ((ProjectRepositoryNode) view.getRoot()).getRootRepositoryNode(repositoryNodeType);
+        IRepositoryNode repositoryNode = view.getRoot().getRootRepositoryNode(ESBRepositoryNodeType.SERVICES);
         if (null != repositoryNode) {
-            // expand/select repository node
             setWorkbenchPart(view);
-            final StructuredViewer viewer = view.getViewer();
-            if (viewer instanceof TreeViewer) {
-                ((TreeViewer) viewer).expandToLevel(repositoryNode, 1);
-            }
-            viewer.setSelection(new StructuredSelection(repositoryNode));
-
             // find node item
-            IRepositoryNode nodeItem = RepositorySeekerManager.getInstance().searchRepoViewNode(nodeItemId, false);
+            IRepositoryNode nodeItem = RepositorySeekerManager.getInstance().searchRepoViewNode(params.getProperty("nodeId"), false);
             if (null != nodeItem) {
                 // expand/select node item
-                if (viewer instanceof TreeViewer) {
-                    ((TreeViewer) viewer).expandToLevel(nodeItem, 2);
-                }
-                viewer.setSelection(new StructuredSelection(nodeItem));
+                view.getViewer().setSelection(new StructuredSelection(nodeItem));
+                init((RepositoryNode) nodeItem);
+                doRun();
             }
         }
     }
+
 }
