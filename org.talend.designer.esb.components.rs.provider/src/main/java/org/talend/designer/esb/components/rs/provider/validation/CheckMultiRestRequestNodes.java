9733867a621d6ea48f94853568546005f9752254
esb_tooling_build <esb_tooling_build@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-08-28 02:12:10 +0000

    ESB_Tooling_SE autocommit - build #828 (d2f45e4fc7) gangliu:  [TESB-10927] Show an error flag on tESBRequestProvider or tRESTRequest component if more than one existing
    git-svn-id: http://talendforge.org/svn/tos/trunk@107208 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.esb.components.rs.provider/src/main/java/org/talend/designer/esb/components/rs/provider/validation/CheckMultiRestRequestNodes.java
+++ org.talend.designer.esb.components.rs.provider/src/main/java/org/talend/designer/esb/components/rs/provider/validation/CheckMultiRestRequestNodes.java
@@ -1,9 +1,6 @@
 package org.talend.designer.esb.components.rs.provider.validation;
 
-import java.util.List;
-
 import org.talend.core.model.process.Element;
-import org.talend.core.model.process.INode;
 import org.talend.core.model.process.IProcess;
 import org.talend.core.model.process.Problem.ProblemStatus;
 import org.talend.designer.core.ICheckNodesService;
@@ -12,7 +9,8 @@ import org.talend.designer.core.ui.views.problems.Problems;
 
 public class CheckMultiRestRequestNodes implements ICheckNodesService {
 
-	private static String COMPONENT_NAME = "tRESTRequest";
+	private static String RESTREQUEST_COMPONENT = "tRESTRequest";
+	private static String ESBPROVIDER_COMPONENT = "tESBProviderRequest";
 	
 	public CheckMultiRestRequestNodes() {
 	}
@@ -20,14 +18,16 @@ public class CheckMultiRestRequestNodes implements ICheckNodesService {
 	@Override
 	public void checkNode(Node node) {
 		String name = node.getComponent().getName();
-		if(!COMPONENT_NAME.equals(name)){
+		if(!RESTREQUEST_COMPONENT.equals(name)){
 			return;
 		}
 		IProcess process = node.getProcess();
-		List<? extends INode> nodesOfType = process.getNodesOfType(COMPONENT_NAME);
-		if(nodesOfType.size()>1){
+		if(process.getNodesOfType(RESTREQUEST_COMPONENT).size()>1){
 			Problems.add(ProblemStatus.ERROR, (Element) node,
-					"Only one "+COMPONENT_NAME+" component is allowed at one Job!");
+					"Only one "+RESTREQUEST_COMPONENT+" component is allowed in a Job!");
+		}else if(process.getNodesOfType(ESBPROVIDER_COMPONENT).size()>0){
+			Problems.add(ProblemStatus.ERROR, (Element) node,
+					RESTREQUEST_COMPONENT+" and "+ESBPROVIDER_COMPONENT+" can't present in a same Job!");
 		}
 	}
 
