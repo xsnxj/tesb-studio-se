0d87702bac384b066e398a543b32c7112bd58b3d
gliu <gliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-02-24 07:05:54 +0000

    [TESB-12898] Studio-Preferences->ESB->Service-REST... [TESB-12966] Investigate and Fix
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_4@114350 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.esb.components.rs.provider/src/main/java/org/talend/designer/esb/components/rs/provider/Activator.java
+++ org.talend.designer.esb.components.rs.provider/src/main/java/org/talend/designer/esb/components/rs/provider/Activator.java
@@ -44,6 +44,7 @@ public class Activator extends AbstractUIPlugin {
     public void start(BundleContext context) throws Exception {
         super.start(context);
         plugin = this;
+        loadCustomProperty();
     }
 
     public void stop(BundleContext context) throws Exception {
