06e0354ea39cc57346277f5f152a23ad83dcea49
esb_tooling_build <esb_tooling_build@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-02-24 10:08:03 +0000

    ESB_Tooling_SE autocommit - build #968 (12a903015e) amarkevich:  added mail dep for route components
    git-svn-id: http://talendforge.org/svn/tos/trunk@114372 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.esb.components.rs.provider/src/main/java/org/talend/designer/esb/components/rs/provider/Activator.java
+++ org.talend.designer.esb.components.rs.provider/src/main/java/org/talend/designer/esb/components/rs/provider/Activator.java
@@ -44,7 +44,6 @@ public class Activator extends AbstractUIPlugin {
     public void start(BundleContext context) throws Exception {
         super.start(context);
         plugin = this;
-        loadCustomProperty();
     }
 
     public void stop(BundleContext context) throws Exception {
