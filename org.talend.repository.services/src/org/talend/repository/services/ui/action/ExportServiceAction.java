b1c38f4b3702bb4a52a8313777bc061f160b192e
gliu <gliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-02-25 09:19:31 +0000

    [TESB-12909] Problems with multiple cTalendJob usage
    git-svn-id: http://talendforge.org/svn/tos/trunk@114427 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository.services/src/org/talend/repository/services/ui/action/ExportServiceAction.java
+++ org.talend.repository.services/src/org/talend/repository/services/ui/action/ExportServiceAction.java
@@ -24,6 +24,7 @@ import org.talend.commons.exception.PersistenceException;
 import org.talend.core.model.properties.ProcessItem;
 import org.talend.core.model.repository.ERepositoryObjectType;
 import org.talend.core.model.repository.IRepositoryViewObject;
+import org.talend.core.model.utils.JavaResourcesHelper;
 import org.talend.core.repository.constants.FileConstants;
 import org.talend.core.repository.model.ProxyRepositoryFactory;
 import org.talend.designer.publish.core.models.BundleModel;
@@ -110,8 +111,7 @@ public class ExportServiceAction implements IRunnableWithProgress {
                         if(jobNode==null) {
                         	continue;
                         }
-                        String jobName = jobNode.getObject().getLabel();
-                        operations.put(operationName, jobName);
+                        operations.put(operationName, JavaResourcesHelper.getJobClassName(jobNode));
                         nodes.add(jobNode);
                     }
                 }
