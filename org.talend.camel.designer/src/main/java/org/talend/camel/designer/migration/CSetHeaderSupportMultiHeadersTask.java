83bb862bf571771daa8aa9ae2e68af23481913f8
zgao <zgao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-04-22 07:31:54 +0000

    [TESB-13416] Studio fails to read routes correctly - cannot display setHeader information correctly and generated illegal code --[TESB-13434] Fix CSetHeaderSupportMultiHeadersTask to pre-process setHeader information. Also renamed the migration task id to make it works on every routes.
    git-svn-id: http://talendforge.org/svn/tos/trunk@116727 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.camel.designer/src/main/java/org/talend/camel/designer/migration/CSetHeaderSupportMultiHeadersTask.java
+++ org.talend.camel.designer/src/main/java/org/talend/camel/designer/migration/CSetHeaderSupportMultiHeadersTask.java
@@ -1,18 +1,18 @@
 package org.talend.camel.designer.migration;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 
-import org.eclipse.emf.common.util.EList;
 import org.talend.commons.exception.PersistenceException;
 import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
 import org.talend.designer.core.model.utils.emf.talendfile.ElementValueType;
 import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
 import org.talend.designer.core.model.utils.emf.talendfile.TalendFileFactory;
 
-public class CSetHeaderSupportMultiHeadersTask extends
-		AbstractRouteItemComponentMigrationTask{
-	
+public class CSetHeaderSupportMultiHeadersTask extends AbstractRouteItemComponentMigrationTask {
+
 	@Override
 	public String getComponentNameRegex() {
 		return "cSetHeader";
@@ -25,12 +25,71 @@ public class CSetHeaderSupportMultiHeadersTask extends
 
 	@Override
 	protected boolean execute(NodeType node) throws Exception {
+		List<?> parameters = node.getElementParameter();
+		List<ElementParameterType> valuesParams = getValuesParams(parameters);
+		if(!valuesParams.isEmpty()) {
+			return updateValuesParams(parameters, valuesParams);
+		}
 		cSetHeaderMigrate(node);
 		return true;
 	}
 
+	/**
+	 * Gets all elementParameter with attribute name={@link #STRING_VALUES} .
+	 *
+	 * @param parameters the parameters
+	 * @return the values params
+	 */
+	private List<ElementParameterType> getValuesParams(List<?> parameters) {
+		List<ElementParameterType> valuesParams = new ArrayList<ElementParameterType>();
+		for (Object tmp : parameters) {
+			ElementParameterType param = (ElementParameterType) tmp;
+			if ("VALUES".equals(param.getName())) {
+				valuesParams.add(param);
+			}
+		}
+		return valuesParams;
+	}
+
+	/**
+	 * Update values params due to duplicated or no attribute field="TABLE".
+	 * Shouldn't happened for normail case but in order to fix [TESB-13416] still do a workaround.
+	 *
+	 * @param parameters the parameters
+	 * @param valuesParams the values params, can't be empty
+	 * @return true, if needs save
+	 */
+	private boolean updateValuesParams(List<?> parameters, List<ElementParameterType> valuesParams) {
+		ElementParameterType keep = null;
+		if(valuesParams.size() == 1) {
+			keep = valuesParams.get(0);
+			if ("TABLE".equals(keep.getField())) {
+				return false;
+			}
+		}else {
+			// duplicated params
+			for (ElementParameterType valuesParam : valuesParams) {
+				if ("TABLE".equals(valuesParam.getField())) {
+					keep = valuesParam;
+					break;
+				}
+			}
+
+			if(keep == null) {
+				keep = valuesParams.remove(0);
+			}else {
+				valuesParams.remove(keep);
+			}
+			parameters.removeAll(valuesParams);
+		}
+		keep.setField("TABLE");
+		return true;
+	
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private void cSetHeaderMigrate(NodeType currentNode) throws PersistenceException {
-		EList parameters = currentNode.getElementParameter();
+		List parameters = currentNode.getElementParameter();
 		String headerName = null;
 		String useBean = null;
 		String expression = null;
@@ -81,6 +140,7 @@ public class CSetHeaderSupportMultiHeadersTask extends
 		ElementParameterType newParameter = TalendFileFactory.eINSTANCE
 				.createElementParameterType();
 		newParameter.setName("VALUES");
+		newParameter.setField("TABLE");
 		newParameter.getElementValue().add(newNameValue);
 		newParameter.getElementValue().add(newLanguageValue);
 		newParameter.getElementValue().add(newExpressionValue);
