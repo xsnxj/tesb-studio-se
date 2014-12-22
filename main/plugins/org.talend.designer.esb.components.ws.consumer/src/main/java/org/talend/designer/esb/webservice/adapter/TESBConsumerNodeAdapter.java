5af1452b70c5adfbf395d32fb794942ecb8143bd
billysun <billysun@talend.com>
2014-12-22 18:14:53 +0800

    TESB-14624: tESBConsumer: do not change Endpoint field if context variable set

--- main/plugins/org.talend.designer.esb.components.ws.consumer/src/main/java/org/talend/designer/esb/webservice/adapter/TESBConsumerNodeAdapter.java
+++ main/plugins/org.talend.designer.esb.components.ws.consumer/src/main/java/org/talend/designer/esb/webservice/adapter/TESBConsumerNodeAdapter.java
@@ -11,17 +11,17 @@ import org.talend.designer.esb.webservice.ServiceSetting;
 import org.talend.designer.esb.webservice.WebServiceNode;
 import org.talend.designer.esb.webservice.ws.wsdlinfo.Function;
 
-class TESBConsumerNodeAdapter extends AbstractNodeAdapter{
+class TESBConsumerNodeAdapter extends AbstractNodeAdapter {
 
-	TESBConsumerNodeAdapter(WebServiceNode node) {
-		super(node);
-	}
+    TESBConsumerNodeAdapter(WebServiceNode node) {
+        super(node);
+    }
 
-	@Override
-	public IStatus setNodeSetting(ServiceSetting setting) {
-    	node.setParamValue("ENDPOINT", setting.getWsdlLocation());
+    @Override
+    public IStatus setNodeSetting(ServiceSetting setting) {
+        node.setParamValue("ENDPOINT", setting.getWsdlLocation());
         try {
-        	String wsdlString = setting.getCompressedAndEncodedWSDL();
+            String wsdlString = setting.getCompressedAndEncodedWSDL();
             node.setParamValue("WSDL_CONTENT", wsdlString);
         } catch (Exception e) {
             throw new IllegalStateException("Unable to create wsdl content", e);
@@ -29,43 +29,46 @@ class TESBConsumerNodeAdapter extends AbstractNodeAdapter{
         Function currentFunction = setting.getFunction();
         node.setParamValue(PORT_NAME, currentFunction.getPortName());
         node.setParamValue("METHOD_NS", currentFunction.getNameSpaceURI());
-		node.setParamValue(METHOD, currentFunction.getName());
+        node.setParamValue(METHOD, currentFunction.getName());
         node.setParamValue(SERVICE_NS, currentFunction.getServiceNameSpace());
         node.setParamValue(SERVICE_NAME, currentFunction.getServiceName());
         node.setParamValue("PORT_NS", currentFunction.getServiceNameSpace());
         node.setParamValue("SOAP_ACTION", currentFunction.getSoapAction());
-        node.setParamValue("ESB_ENDPOINT", TalendTextUtils.addQuotes(currentFunction.getAddressLocation()));
+        if (setting.getWsdlLocation().startsWith("context.")) {
+            node.setParamValue("ESB_ENDPOINT", setting.getWsdlLocation());
+        } else {
+            node.setParamValue("ESB_ENDPOINT", TalendTextUtils.addQuotes(currentFunction.getAddressLocation()));
+        }
         node.setParamValue("COMMUNICATION_STYLE", currentFunction.getCommunicationStyle());
-		return Status.OK_STATUS;
-	}
+        return Status.OK_STATUS;
+    }
 
-	@Override
-	public Function loadCurrentFunction() {
-		String operationName = node.getParamStringValue(METHOD);
-		if(operationName == null) {
-			return null;
-		}
-		String portName = node.getParamStringValue(PORT_NAME);
-		if(portName == null) {
-			return null;
-		}
-		String serviceName = node.getParamStringValue(SERVICE_NAME);
-		String targetNamespace = node.getParamStringValue(SERVICE_NS);
-		if(serviceName == null || targetNamespace == null) {
-			return null;
-		}
-		QName serviceQName = new QName(targetNamespace,
-				serviceName);
-		return new Function(operationName, portName, serviceQName);
-	}
+    @Override
+    public Function loadCurrentFunction() {
+        String operationName = node.getParamStringValue(METHOD);
+        if (operationName == null) {
+            return null;
+        }
+        String portName = node.getParamStringValue(PORT_NAME);
+        if (portName == null) {
+            return null;
+        }
+        String serviceName = node.getParamStringValue(SERVICE_NAME);
+        String targetNamespace = node.getParamStringValue(SERVICE_NS);
+        if (serviceName == null || targetNamespace == null) {
+            return null;
+        }
+        QName serviceQName = new QName(targetNamespace, serviceName);
+        return new Function(operationName, portName, serviceQName);
+    }
 
-	@Override
-	public String getInitialWsdlLocation() {
-		return node.getParamStringValue(ENDPOINT);
-	}
+    @Override
+    public String getInitialWsdlLocation() {
+        return node.getParamStringValue(ENDPOINT);
+    }
 
-	@Override
-	public boolean allowPopulateSchema() {
-		return true;
-	}
+    @Override
+    public boolean allowPopulateSchema() {
+        return true;
+    }
 }
