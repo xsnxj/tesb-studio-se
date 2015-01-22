bcbe521f14c9c840ac753899a656a3b2d3d16ed5
billy sun <csun@talend.com>
2015-01-22 11:57:14 +0800

    TESB-14376: Job with tRouteInput component can not be detected in cTalendJob

--- main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/ui/wizards/AssignJobPage.java
+++ main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/ui/wizards/AssignJobPage.java
@@ -13,6 +13,9 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 import org.talend.camel.designer.i18n.Messages;
 import org.talend.camel.designer.util.CamelDesignerUtil;
 import org.talend.core.GlobalServiceRegister;
@@ -201,6 +204,22 @@ public class AssignJobPage extends WizardPage {
 
         @Override
         public Control createDialogArea(Composite parent) {
+
+            IWorkbenchWindow activeWorkbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+
+            if (activeWorkbenchWindow != null) {
+                IWorkbenchPage activePage = activeWorkbenchWindow.getActivePage();
+
+                if (activePage != null) {
+
+                    String perspectiveId = activePage.getPerspective().getId();
+                    if ("org.talend.camel.perspective".equals(perspectiveId)) {
+                        return super.createDialogArea(parent, "org.talend.rcp.perspective");
+                    }
+
+                }
+            }
+
             return super.createDialogArea(parent);
         }
 
