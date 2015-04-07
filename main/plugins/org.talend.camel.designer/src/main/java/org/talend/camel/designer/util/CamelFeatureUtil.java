4306ff4652947a0ccf92d7c58cf8bf8b91de641d
amarkevich <amarkevich@talend.com>
2015-04-07 05:36:47 -0300

    embded commons-net

--- main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/util/CamelFeatureUtil.java
+++ main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/util/CamelFeatureUtil.java
@@ -12,13 +12,11 @@
 // ============================================================================
 package org.talend.camel.designer.util;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 import org.eclipse.emf.common.util.EList;
