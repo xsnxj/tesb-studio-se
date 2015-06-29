bebb3f1a520dfa17f82d6d49d108060035a0175f
SunChaoqun <csun@talend.com>
2015-06-29 14:11:48 +0800

    TESB-15643: The services under service node in integration perspective can not show in the cCXF Service configuration wizard

--- main/plugins/org.talend.designer.esb.components.ws.consumer/src/main/java/org/talend/designer/esb/webservice/ui/ServicesButtonPart.java
+++ main/plugins/org.talend.designer.esb.components.ws.consumer/src/main/java/org/talend/designer/esb/webservice/ui/ServicesButtonPart.java
@@ -19,57 +19,65 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.talend.core.model.repository.ERepositoryObjectType;
 import org.talend.repository.model.RepositoryNode;
 import org.talend.repository.ui.dialog.RepositoryReviewDialog;
 
-interface ServiceSelectionListener extends EventListener{
-	void serviceNodeSelected(RepositoryNode serviceNode);
+interface ServiceSelectionListener extends EventListener {
+
+    void serviceNodeSelected(RepositoryNode serviceNode);
 }
 
 /**
- * Provide a dialog to select a service node from Services.
- * (separated from {@link WebServiceUI}
+ * Provide a dialog to select a service node from Services. (separated from {@link WebServiceUI}
  * 
  * @author GaoZone
  */
 public class ServicesButtonPart extends AbstractButtonPart<ServiceSelectionListener> {
 
-	public ServicesButtonPart(ServiceSelectionListener eventListener) {
-		super(eventListener);
-	}
+    public ServicesButtonPart(ServiceSelectionListener eventListener) {
+        super(eventListener);
+    }
 
-	@Override
-	protected void buttonSelected(SelectionEvent e) {
-		RepositoryReviewDialog dialog = new RepositoryReviewDialog(getShell(),
-				ERepositoryObjectType.METADATA, "SERVICES:OPERATION") {
-			@Override
-			protected boolean isSelectionValid(SelectionChangedEvent event) {
-				IStructuredSelection selection = (IStructuredSelection) event.getSelection();
-				if (selection.size() == 1) {
-					return true;
-				}
-				return false;
-			}
-		};
+    @Override
+    protected void buttonSelected(SelectionEvent e) {
+        RepositoryReviewDialog dialog = new RepositoryReviewDialog(getShell(), ERepositoryObjectType.METADATA,
+                "SERVICES:OPERATION") {
 
-		int open = dialog.open();
-		if (open == Dialog.OK) {
-			RepositoryNode result = dialog.getResult();
-			if(result != null) {
-				listener.serviceNodeSelected(result);
-			}
-		}
-	}
+            @Override
+            protected boolean isSelectionValid(SelectionChangedEvent event) {
+                IStructuredSelection selection = (IStructuredSelection) event.getSelection();
+                if (selection.size() == 1) {
+                    return true;
+                }
+                return false;
+            }
 
-	@Override
-	protected String getMessageKey() {
-		return "WebServiceUI.Services";
-	}
+            @Override
+            protected Control createDialogArea(Composite parent) {
+                return createDialogArea(parent, "org.talend.rcp.perspective");
+            }
+        };
 
-	@Override
-	protected Image getImage() {
-		return getImageFromBundle("org.talend.repository.services", "icons/services.png");
-	}
+        int open = dialog.open();
+        if (open == Dialog.OK) {
+            RepositoryNode result = dialog.getResult();
+            if (result != null) {
+                listener.serviceNodeSelected(result);
+            }
+        }
+    }
+
+    @Override
+    protected String getMessageKey() {
+        return "WebServiceUI.Services";
+    }
+
+    @Override
+    protected Image getImage() {
+        return getImageFromBundle("org.talend.repository.services", "icons/services.png");
+    }
 
 }
