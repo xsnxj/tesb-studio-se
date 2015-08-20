9d4eef8a3507f1ac01465f0d810814681c4ad785
amarkevich <amarkevich@talend.com>
2015-08-20 14:38:32 +0300

    reuse RunStat instead of CamelStat

--- main/plugins/org.talend.designer.camel.components.localprovider/src/main/java/org/talend/designer/camel/components/localprovider/CamelComponentPlugin.java
+++ main/plugins/org.talend.designer.camel.components.localprovider/src/main/java/org/talend/designer/camel/components/localprovider/CamelComponentPlugin.java
@@ -20,25 +20,13 @@ import org.osgi.framework.BundleContext;
  */
 public class CamelComponentPlugin extends AbstractUIPlugin {
 
-    public static final String PLUGIN_ID = "org.talend.designer.camel.components.localprovider";
-
     private static CamelComponentPlugin plugin;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-     */
     public void start(BundleContext context) throws Exception {
         super.start(context);
         plugin = this;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-     */
     public void stop(BundleContext context) throws Exception {
         plugin = null;
         super.stop(context);
@@ -52,4 +40,5 @@ public class CamelComponentPlugin extends AbstractUIPlugin {
     public static CamelComponentPlugin getDefault() {
         return plugin;
     }
+
 }
--- main/plugins/org.talend.designer.camel.components.localprovider/src/main/java/org/talend/designer/camel/components/localprovider/CamelLocalComponentsProvider.java
+++ main/plugins/org.talend.designer.camel.components.localprovider/src/main/java/org/talend/designer/camel/components/localprovider/CamelLocalComponentsProvider.java
@@ -17,21 +17,13 @@ import java.net.URL;
 
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
-import org.talend.core.GlobalServiceRegister;
 import org.talend.core.model.components.AbstractComponentsProvider;
-import org.talend.core.model.components.IComponentsFactory;
-import org.talend.core.ui.branding.IBrandingService;
 
 /**
  * DOC guanglong.du class global comment. Detailled comment
  */
 public class CamelLocalComponentsProvider extends AbstractComponentsProvider {
 
-    /*
-     * (non-Jsdoc)
-     * 
-     * @see org.talend.core.model.components.AbstractComponentsProvider#getExternalComponentsLocation()
-     */
     protected File getExternalComponentsLocation() {
         URL url = FileLocator.find(CamelComponentPlugin.getDefault().getBundle(), new Path("components"), null); //$NON-NLS-1$
         URL fileUrl;
@@ -43,44 +35,4 @@ public class CamelLocalComponentsProvider extends AbstractComponentsProvider {
         return null;
     }
 
-    // public void preComponentsLoad() throws IOException {
-    // File installationFolder = getInstallationFolder();
-    //
-    // FilesUtils.createFoldersIfNotExists(installationFolder.getAbsolutePath(), false);
-    //
-    // File externalComponentsLocation = getExternalComponentsLocation();
-    // if (externalComponentsLocation != null) {
-    // if (externalComponentsLocation.exists()) {
-    // try {
-    // FileFilter ff = new FileFilter() {
-    //
-    // public boolean accept(File pathname) {
-    // if (pathname.getName().equals(".svn")) {
-    // return false;
-    // }
-    // return true;
-    // }
-    //
-    // };
-    // FilesUtils.copyFolder(externalComponentsLocation, installationFolder, false, ff, null, true, true);
-    // } catch (IOException e) {
-    // ExceptionHandler.process(e);
-    // }
-    // } else {
-    // logger.warn(Messages
-    //                        .getString("AbstractComponentsProvider.folderNotExist", externalComponentsLocation.toString())); //$NON-NLS-1$
-    // }
-    // }
-    // }
-
-    @Override
-    public String getComponentsLocation() {
-        IBrandingService brandingService = (IBrandingService) GlobalServiceRegister.getDefault().getService(
-                IBrandingService.class);
-        if (brandingService.isPoweredOnlyCamel()) {
-            return new Path(IComponentsFactory.COMPONENTS_INNER_FOLDER).toString();
-        } else {
-            return super.getComponentsLocation();
-        }
-    }
 }
