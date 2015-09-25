8b753e3ed3612de4aca782e1deefa33fd12c1f29
amarkevich <amarkevich@talend.com>
2015-09-25 13:32:37 +0300

    TESB-16469 Generate unique component Id

--- main/plugins/org.talend.camel.designer.codegen/src/main/java/org/talend/camel/designer/codegen/partgen/EndpointIdGenerator.java
+++ main/plugins/org.talend.camel.designer.codegen/src/main/java/org/talend/camel/designer/codegen/partgen/EndpointIdGenerator.java
@@ -1,42 +1,37 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package org.talend.camel.designer.codegen.partgen;
 
 import org.talend.camel.designer.codegen.util.NodeUtil;
-import org.talend.core.model.process.IElementParameter;
 import org.talend.core.model.process.INode;
 import org.talend.designer.codegen.exception.CodeGeneratorException;
 
 public class EndpointIdGenerator implements PartGenerator<INode> {
 
-	@Override
-	public CharSequence generatePart(INode node, Object... ignoredParams) throws CodeGeneratorException {
-		return generateEndpointId(node);
-	}
+    private static final String getNodeId(INode node) {
+        return node.getProcess().getName() + '_' + node.getUniqueName();
+    }
 
-	private final String generateEndpointId(INode node) {
-		if (NodeUtil.isStartNode(node)) {
+    @Override
+	public CharSequence generatePart(INode node, Object... ignoredParams) throws CodeGeneratorException {
+        if (NodeUtil.isStartNode(node)) {
             if ("cErrorHandler".equals(node.getComponent().getName())) { //$NON-NLS-1$
                 return ""; //$NON-NLS-1$
             }
-			// http://jira.talendforge.org/browse/TESB-4086
-			// XiaopengLi
-			String label = null;
-			IElementParameter parameter = node.getElementParameter("LABEL"); //$NON-NLS-1$
-			if (parameter != null && !"__UNIQUE_NAME__".equals(parameter.getValue())) { //$NON-NLS-1$
-				label = (String) parameter.getValue();
-			}
-
-			/*
-			 * Fix https://jira.talendforge.org/browse/TESB-6685 label +
-			 * uniqueName to make it unique
-			 */
-			if (label == null) {
-				label = node.getUniqueName();
-			} else {
-				label += '_' + node.getUniqueName();
-			}
-			return ".routeId(\"" + label + "\")"; //$NON-NLS-1$ //$NON-NLS-2$
-		} else {
-			return ".id(\"" + node.getUniqueName() + "\")"; //$NON-NLS-1$ //$NON-NLS-2$
-		}
+            return ".routeId(\"" + getNodeId(node) + "\")"; //$NON-NLS-1$ //$NON-NLS-2$
+        } else {
+            return ".id(\"" + getNodeId(node) + "\")"; //$NON-NLS-1$ //$NON-NLS-2$
+        }
 	}
+
 }
