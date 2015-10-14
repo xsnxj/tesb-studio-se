3590fe6493ee6759ac1c8016b2ef4c0b3eb505e9
amarkevich <amarkevich@talend.com>
2015-10-14 11:31:40 +0300

    TESB-16573 [Routelets Resource] Resource should not be deleted if it is used by routelet

--- main/plugins/org.talend.designer.camel.resource/src/org/talend/designer/camel/resource/delete/CheckRouteResourceDeleteReference.java
+++ main/plugins/org.talend.designer.camel.resource/src/org/talend/designer/camel/resource/delete/CheckRouteResourceDeleteReference.java
@@ -14,18 +14,18 @@ package org.talend.designer.camel.resource.delete;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.talend.camel.model.CamelRepositoryNodeType;
+import org.talend.commons.exception.ExceptionHandler;
 import org.talend.commons.exception.PersistenceException;
 import org.talend.core.model.properties.Item;
 import org.talend.core.model.properties.ProcessItem;
 import org.talend.core.model.properties.Property;
+import org.talend.core.model.repository.ERepositoryObjectType;
 import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.core.repository.model.AbstractCheckDeleteItemReference;
 import org.talend.core.repository.model.ItemReferenceBean;
-import org.talend.core.repository.model.provider.ICheckDeleteItemReference;
 import org.talend.core.repository.ui.actions.DeleteActionCache;
 import org.talend.designer.camel.resource.core.model.ResourceDependencyModel;
 import org.talend.designer.camel.resource.core.util.RouteResourceUtil;
@@ -35,64 +35,52 @@ import org.talend.repository.model.IProxyRepositoryFactory;
  * @author xpli
  * 
  */
-public class CheckRouteResourceDeleteReference extends
-		AbstractCheckDeleteItemReference implements ICheckDeleteItemReference {
+public class CheckRouteResourceDeleteReference extends AbstractCheckDeleteItemReference {
 
-	@Override
-	protected Set<ItemReferenceBean> checkItemReferenceBeans(
-			IProxyRepositoryFactory factory,
-			DeleteActionCache deleteActionCache, IRepositoryViewObject object) {
-		Item nodeItem = object.getProperty().getItem();
-		Set<ItemReferenceBean> list = new HashSet<ItemReferenceBean>();
-		List<IRepositoryViewObject> allRoutes;
-		try {
-			allRoutes = factory
-					.getAll(CamelRepositoryNodeType.repositoryRoutesType);
-			if (allRoutes == null || allRoutes.isEmpty()) {
-				return list;
-			}
-			for (IRepositoryViewObject obj : allRoutes) {
-				Property property = obj.getProperty();
-				Item routeItem = property.getItem();
-				Collection<ResourceDependencyModel> models = RouteResourceUtil
-						.getResourceDependencies((ProcessItem) routeItem);
-				for (ResourceDependencyModel model : models) {
+    @Override
+    protected Set<ItemReferenceBean> checkItemReferenceBeans(IProxyRepositoryFactory factory,
+        DeleteActionCache deleteActionCache, IRepositoryViewObject object) {
+        final Set<ItemReferenceBean> list = new HashSet<ItemReferenceBean>(
+            collect(factory, object, CamelRepositoryNodeType.repositoryRoutesType));
+        list.addAll(collect(factory, object, CamelRepositoryNodeType.repositoryRouteletType));
+        return list;
+    }
 
-					if (nodeItem.getProperty().getId()
-							.equals(model.getItem().getProperty().getId())) {
-
-						ItemReferenceBean bean = new ItemReferenceBean();
-						bean.setItemName(object.getProperty().getLabel());
-						bean.setItemVersion(model.getSelectedVersion());
-						bean.setItemType(CamelRepositoryNodeType.repositoryRouteResourceType);
-						bean.setItemDeleted(object.isDeleted());
-						bean.setReferenceItemName(property.getLabel());
-						bean.setReferenceItemVersion(property.getVersion());
-						bean.setReferenceItemType(CamelRepositoryNodeType.repositoryRoutesType);
-						bean.setReferenceItemPath(routeItem.getState()
-								.getPath());
-						bean.setReferenceProjectName(obj.getProjectLabel());
-						bean.setReferenceItemDeleted(obj.isDeleted());
-						boolean found = false;
-						for (ItemReferenceBean b : list) {
-							if (b.equals(bean)) {
-								found = true;
-								b.addNodeNum();
-								break;
-							}
-						}
-						if (!found) {
-							list.add(bean);
-						}
-					}
-
-				}
-			}
-		} catch (PersistenceException e1) {
-			e1.printStackTrace();
-		}
-
-		return list;
-	}
-
+    private Collection<ItemReferenceBean> collect(IProxyRepositoryFactory factory,
+        IRepositoryViewObject object, ERepositoryObjectType type) {
+        final Item nodeItem = object.getProperty().getItem();
+        final Set<ItemReferenceBean> list = new HashSet<ItemReferenceBean>();
+        try {
+            for (IRepositoryViewObject obj : factory.getAll(type)) {
+                final Property property = obj.getProperty();
+                final Item routeItem = property.getItem();
+                for (ResourceDependencyModel model : RouteResourceUtil.getResourceDependencies((ProcessItem) routeItem)) {
+                    if (nodeItem.getProperty().getId().equals(model.getItem().getProperty().getId())) {
+                        final ItemReferenceBean bean = new ItemReferenceBean();
+                        bean.setItemName(object.getProperty().getLabel());
+                        bean.setItemVersion(model.getSelectedVersion());
+                        bean.setItemType(CamelRepositoryNodeType.repositoryRouteResourceType);
+                        bean.setItemDeleted(object.isDeleted());
+                        bean.setReferenceItemName(property.getLabel());
+                        bean.setReferenceItemVersion(property.getVersion());
+                        bean.setReferenceItemType(type);
+                        bean.setReferenceItemPath(routeItem.getState().getPath());
+                        bean.setReferenceProjectName(obj.getProjectLabel());
+                        bean.setReferenceItemDeleted(obj.isDeleted());
+                        if (!list.add(bean)) {
+                            for (ItemReferenceBean b : list) {
+                                if (b.equals(bean)) {
+                                    b.addNodeNum();
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (PersistenceException e) {
+            ExceptionHandler.process(e);
+        }
+        return list;
+    }
 }
