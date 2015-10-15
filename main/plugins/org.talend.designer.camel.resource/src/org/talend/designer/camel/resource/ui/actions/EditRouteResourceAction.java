7a5e1d4364a3ecc9f8918d15a3634fb362893238
amarkevich <amarkevich@talend.com>
2015-10-15 11:27:46 +0300

    TESB-16571 [Resources] Edit Resource does not display in right menu, only display icon

--- main/plugins/org.talend.designer.camel.resource/src/org/talend/designer/camel/resource/ui/actions/EditRouteResourceAction.java
+++ main/plugins/org.talend.designer.camel.resource/src/org/talend/designer/camel/resource/ui/actions/EditRouteResourceAction.java
@@ -19,17 +19,14 @@ import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.ui.IWorkbenchPage;
 import org.talend.camel.core.model.camelProperties.RouteResourceItem;
 import org.talend.camel.model.CamelRepositoryNodeType;
+import org.talend.commons.runtime.model.repository.ERepositoryStatus;
 import org.talend.core.model.properties.Property;
 import org.talend.core.repository.model.ProxyRepositoryFactory;
 import org.talend.designer.camel.resource.RouteResourceActivator;
 import org.talend.designer.camel.resource.i18n.Messages;
 import org.talend.designer.camel.resource.ui.util.RouteResourceEditorUtil;
-import org.talend.designer.core.DesignerPlugin;
 import org.talend.repository.ProjectManager;
-import org.talend.repository.model.BinRepositoryNode;
-import org.talend.repository.model.IProxyRepositoryFactory;
-import org.talend.repository.model.IRepositoryService;
-import org.talend.repository.model.RepositoryNode;
+import org.talend.repository.model.IRepositoryNode;
 import org.talend.repository.ui.actions.AContextualAction;
 
 /**
@@ -46,11 +43,7 @@ public class EditRouteResourceAction extends AContextualAction {
         setImageDescriptor(RouteResourceActivator.createImageDesc("icons/edit-resource.png"));
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.jface.action.Action#run()
-	 */
+	@Override
 	protected void doRun() {
 		ISelection selection = getSelection();
 		if (selection == null) {
@@ -60,91 +53,50 @@ public class EditRouteResourceAction extends AContextualAction {
 		if (obj == null) {
 			return;
 		}
-		RepositoryNode node = (RepositoryNode) obj;
-		openOrBindEditor(node);
+		openOrBindEditor((IRepositoryNode) obj);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.talend.repository.ui.actions.AContextualView#getClassForDoubleClick()
-	 */
-	@SuppressWarnings("rawtypes")
 	@Override
-	public Class getClassForDoubleClick() {
+	public Class<?> getClassForDoubleClick() {
 		return RouteResourceItem.class;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.talend.repository.ui.actions.ITreeContextualAction#init(org.eclipse
-	 * .jface.viewers.TreeViewer,
-	 * org.eclipse.jface.viewers.IStructuredSelection)
-	 */
-	public void init(TreeViewer viewer, IStructuredSelection selection) {
+    @Override
+    public void init(TreeViewer viewer, IStructuredSelection selection) {
         boolean canWork = !selection.isEmpty() && (selection.size() == 1)
             && !ProxyRepositoryFactory.getInstance().isUserReadOnlyOnCurrentProject();
-		if (canWork) {
-			Object o = selection.getFirstElement();
-			RepositoryNode node = (RepositoryNode) o;
-			switch (node.getType()) {
-			case REPOSITORY_ELEMENT:
-				if (node.getObjectType() != CamelRepositoryNodeType.repositoryRouteResourceType) {
-					canWork = false;
-				} else {
-					IRepositoryService service = DesignerPlugin.getDefault()
-							.getRepositoryService();
-					IProxyRepositoryFactory repFactory = service
-							.getProxyRepositoryFactory();
-					if (repFactory.isPotentiallyEditable(node.getObject())) {
-						this.setText(""); //$NON-NLS-1$
-					} else {
-						this.setText(""); //$NON-NLS-1$
-					}
-				}
-				break;
-			default:
-				canWork = false;
-			}
-			RepositoryNode parent = node.getParent();
-			if (canWork && parent != null
-					&& parent instanceof BinRepositoryNode) {
-				canWork = false;
-			}
-			if (canWork
-					&& !ProjectManager.getInstance().isInCurrentMainProject(
-							node)) {
-				canWork = false;
-			}
+        if (canWork) {
+            final IRepositoryNode node = (IRepositoryNode) selection.getFirstElement();
+            canWork = node.getObjectType() == CamelRepositoryNodeType.repositoryRouteResourceType;
+            if (canWork && node.getObject() != null
+                && ProxyRepositoryFactory.getInstance().getStatus(node.getObject()) == ERepositoryStatus.DELETED) {
+                canWork = false;
+            }
+            if (canWork && !ProjectManager.getInstance().isInCurrentMainProject(node)) {
+                canWork = false;
+            }
 
-			// If the editProcess action canwork is true, then detect that the
-			// job version is the latest verison or not.
-			if (canWork) {
-				canWork = isLastVersion(node);
-			}
+            // If the editProcess action canwork is true, then detect that the
+            // job version is the latest verison or not.
+            if (canWork) {
+                canWork = isLastVersion(node);
+            }
 
-		}
-		setEnabled(canWork);
-	}
+        }
+        setEnabled(canWork);
+    }
 
 	/**
 	 * Open or bind RouteResourceEditor
 	 * 
 	 * @param node
 	 */
-	private void openOrBindEditor(RepositoryNode node) {
-
-		Property property = (Property) node.getObject().getProperty();
-		RouteResourceItem item = null;
+	private void openOrBindEditor(IRepositoryNode node) {
+		final Property property = (Property) node.getObject().getProperty();
 		if (property != null) {
-
 			Assert.isTrue(property.getItem() instanceof RouteResourceItem);
-			item = (RouteResourceItem) property.getItem();
-			IWorkbenchPage page = getActivePage();
-			RouteResourceEditorUtil.openEditor(page, node, item);
+			final RouteResourceItem item = (RouteResourceItem) property.getItem();
+			RouteResourceEditorUtil.openEditor(getActivePage(), node, item);
 		}
 
 	}
