e58b5485b2c3badce0b7d1a868b7ebb5df2c20ad
SunChaoqun <csun@talend.com>
2016-04-11 17:57:29 +0800

    TESB-17085:cCxf cannot browse wsdl file

--- main/plugins/org.talend.designer.esb.components.ws.consumer/src/main/java/org/talend/designer/esb/webservice/ui/WebServiceUIPresenter.java
+++ main/plugins/org.talend.designer.esb.components.ws.consumer/src/main/java/org/talend/designer/esb/webservice/ui/WebServiceUIPresenter.java
@@ -29,172 +29,176 @@ import org.talend.designer.esb.webservice.ws.wsdlutil.ComponentBuilder;
 
 public class WebServiceUIPresenter implements WsdlFieldListener, ServicePortSelectionListener, FunctionSelectionListener {
 
-	private WebServiceUI webServiceUI;
-	private ServiceSetting currentSetting;
-	private AbstractNodeAdapter nodeAdapter;
+    private WebServiceUI webServiceUI;
 
-	private ExtendedTableModel<Function> functionTableModel;
-	private ExtendedTableModel<String> portTableModel;
-	private Map<String, List<Function>> portFunctionsMap;
+    private ServiceSetting currentSetting;
 
-	WebServiceUIPresenter(WebServiceUI webServiceUI, WebServiceNode webServiceNode) {
-		this.webServiceUI = webServiceUI;
-		this.currentSetting = new ServiceSetting();
-		nodeAdapter = AbstractNodeAdapter.getAdapter(webServiceNode);
+    private AbstractNodeAdapter nodeAdapter;
 
-		List<Function> functionList = new ArrayList<Function>();
-		functionTableModel = new ExtendedTableModel<Function>(null, functionList);
-		portTableModel = new ExtendedTableModel<String>(null, new ArrayList<String>());
-	}
+    private ExtendedTableModel<Function> functionTableModel;
 
-	void initWithCurrentSetting() {
-		Function function = nodeAdapter.loadCurrentFunction();
-		if (function != null) {
-			String port = function.getPortName();
-			currentSetting.setPort(port);
-			currentSetting.setFunction(function);
-			functionTableModel.add(function);
-			portTableModel.add(port);
-			portFunctionsMap = Collections.singletonMap(port, Collections.singletonList(function));
-		}
-	}
+    private ExtendedTableModel<String> portTableModel;
 
-	public void retrieveData(String wsdlLocation) throws WSDLException, InvocationTargetException {
-		String wsdlLocationTemp = TalendQuoteUtils.removeQuotesIfExist(wsdlLocation);
-                Definition definition = nodeAdapter.generateDefinition(wsdlLocationTemp);
-		currentSetting.setWsdlLocation(wsdlLocation);
-		currentSetting.setDefinition(definition);
-		List<Function> functionsAvailable = new ArrayList<Function>();
-		boolean hasRpcOperation = false;
-		for (ServiceInfo serviceInfo : ComponentBuilder.buildModel(definition)) {
-			if (serviceInfo.isHasRpcOperation()) {
-				hasRpcOperation = true;
-			}
-			for (OperationInfo oper : serviceInfo.getOperations()) {
-				Function f = new Function(serviceInfo, oper);
-				functionsAvailable.add(f);
-			}
-		}
-		currentSetting.setHasRcpOperation(hasRpcOperation);
-		
-		portFunctionsMap = new LinkedHashMap<String, List<Function>>();
-		for (Function f : functionsAvailable) {
-			List<Function> functions = portFunctionsMap.get(f.getPortName());
-			if (functions == null) {
-				functions = new ArrayList<Function>();
-				portFunctionsMap.put(f.getPortName(), functions);
-			}
-			functions.add(f);
-		}
-	}
+    private Map<String, List<Function>> portFunctionsMap;
 
-	@Override
-	public ExtendedTableModel<String> getPortTableModel() {
-		return portTableModel;
-	}
+    WebServiceUIPresenter(WebServiceUI webServiceUI, WebServiceNode webServiceNode) {
+        this.webServiceUI = webServiceUI;
+        this.currentSetting = new ServiceSetting();
+        nodeAdapter = AbstractNodeAdapter.getAdapter(webServiceNode);
 
-	@Override
-	public ExtendedTableModel<Function> getFunctionTableModel() {
-		return functionTableModel;
-	}
+        List<Function> functionList = new ArrayList<Function>();
+        functionTableModel = new ExtendedTableModel<Function>(null, functionList);
+        portTableModel = new ExtendedTableModel<String>(null, new ArrayList<String>());
+    }
 
-	@Override
-	public void portSelected(String portName) {
-		currentSetting.setPort(portName);
-		functionTableModel.removeAll();
-		functionTableModel.addAll(portFunctionsMap.get(portName));
-		webServiceUI.selectFirstFunction();
-	}
+    void initWithCurrentSetting() {
+        Function function = nodeAdapter.loadCurrentFunction();
+        if (function != null) {
+            String port = function.getPortName();
+            currentSetting.setPort(port);
+            currentSetting.setFunction(function);
+            functionTableModel.add(function);
+            portTableModel.add(port);
+            portFunctionsMap = Collections.singletonMap(port, Collections.singletonList(function));
+        }
+    }
 
-	@Override
-	public void refreshPageByWsdl(final String wsdlLocation) {
-		webServiceUI.setErrorMessage(null);
-		portTableModel.removeAll();
-		functionTableModel.removeAll();
+    public void retrieveData(String wsdlLocation) throws WSDLException, InvocationTargetException {
+        String wsdlLocationTemp = TalendQuoteUtils.removeQuotesIfExist(wsdlLocation);
+        Definition definition = nodeAdapter.generateDefinition(wsdlLocationTemp);
+        currentSetting.setWsdlLocation(TalendQuoteUtils.addQuotes(wsdlLocationTemp));
+        currentSetting.setDefinition(definition);
+        List<Function> functionsAvailable = new ArrayList<Function>();
+        boolean hasRpcOperation = false;
+        for (ServiceInfo serviceInfo : ComponentBuilder.buildModel(definition)) {
+            if (serviceInfo.isHasRpcOperation()) {
+                hasRpcOperation = true;
+            }
+            for (OperationInfo oper : serviceInfo.getOperations()) {
+                Function f = new Function(serviceInfo, oper);
+                functionsAvailable.add(f);
+            }
+        }
+        currentSetting.setHasRcpOperation(hasRpcOperation);
 
-		IRunnableWithProgress retrieveData = new IRunnableWithProgress() {
+        portFunctionsMap = new LinkedHashMap<String, List<Function>>();
+        for (Function f : functionsAvailable) {
+            List<Function> functions = portFunctionsMap.get(f.getPortName());
+            if (functions == null) {
+                functions = new ArrayList<Function>();
+                portFunctionsMap.put(f.getPortName(), functions);
+            }
+            functions.add(f);
+        }
+    }
 
-			@Override
-			public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-				monitor.beginTask("Retrieve WSDL parameter from net.", IProgressMonitor.UNKNOWN);
-				try {
-					retrieveData(wsdlLocation);
-				} catch (Exception e) {
-					throw new InvocationTargetException(e);
-				} finally {
-					monitor.done();
-				}
-			}
-		};
-		try {
-			webServiceUI.runWithProgress(retrieveData);
-			if (currentSetting.hasRpcOperation()) {
-				webServiceUI.setErrorMessage(Messages.getString("WebServiceUI.NotSupportRpc"));
-			}
-			
-			final List<String> ports = new ArrayList<String>(portFunctionsMap.keySet());
-			portTableModel.addAll(ports);
-			if (!ports.isEmpty()) {
-				functionTableModel.addAll(portFunctionsMap.get(ports.get(0)));
-				webServiceUI.selectFirstFunction();
-			}
-		} catch (InvocationTargetException e) {
-			webServiceUI.setErrorMessage("Error getting service description: " + e.getCause().getMessage());
-			webServiceUI.setPageComplete(false);
-		} catch (InterruptedException e) {
-			// ignore e.
-		}
+    @Override
+    public ExtendedTableModel<String> getPortTableModel() {
+        return portTableModel;
+    }
 
-	}
+    @Override
+    public ExtendedTableModel<Function> getFunctionTableModel() {
+        return functionTableModel;
+    }
 
-	public IStatus performFinishWithFunction(Function function) {
-		if(function == null) {
-			//select default function to get other port infos from it.
-			function = functionTableModel.getBeansList().get(0);
-		}
-		currentSetting.setFunction(function);
-		if (!currentSetting.isUpdated()) {
-			return Status.OK_STATUS;
-		}
-		IStatus status = nodeAdapter.setNodeSetting(currentSetting);
-		if (status.isOK() && webServiceUI.needPopulateSchema()) {
-			status = populateSchema();
-		}
-		return status;
-	}
+    @Override
+    public void portSelected(String portName) {
+        currentSetting.setPort(portName);
+        functionTableModel.removeAll();
+        functionTableModel.addAll(portFunctionsMap.get(portName));
+        webServiceUI.selectFirstFunction();
+    }
 
-	private IStatus populateSchema() {
-		if (currentSetting.isUpdated()) {
-			return SchemaTool.populateSchema(webServiceUI, currentSetting.getDefinition());
-		}
-		return Status.OK_STATUS;
-	}
+    @Override
+    public void refreshPageByWsdl(final String wsdlLocation) {
+        webServiceUI.setErrorMessage(null);
+        portTableModel.removeAll();
+        functionTableModel.removeAll();
 
-	public String getInitialWsdlLocation() {
-		return nodeAdapter.getInitialWsdlLocation();
-	}
-	
-	public boolean isFunctionRequired() {
-		return nodeAdapter.isServiceOperationRequired();
-	}
+        IRunnableWithProgress retrieveData = new IRunnableWithProgress() {
 
-	public boolean allowPopulateSchema() {
-		if(WebServiceComponentPlugin.hasRepositoryServices()) {
-			return nodeAdapter.allowPopulateSchema();
-		}
-		return false;
-	}
+            @Override
+            public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+                monitor.beginTask("Retrieve WSDL parameter from net.", IProgressMonitor.UNKNOWN);
+                try {
+                    retrieveData(wsdlLocation);
+                } catch (Exception e) {
+                    throw new InvocationTargetException(e);
+                } finally {
+                    monitor.done();
+                }
+            }
+        };
+        try {
+            webServiceUI.runWithProgress(retrieveData);
+            if (currentSetting.hasRpcOperation()) {
+                webServiceUI.setErrorMessage(Messages.getString("WebServiceUI.NotSupportRpc"));
+            }
 
-	public boolean showResourcesButton() {
-		return nodeAdapter.routeResourcesAvailable();
-	}
+            final List<String> ports = new ArrayList<String>(portFunctionsMap.keySet());
+            portTableModel.addAll(ports);
+            if (!ports.isEmpty()) {
+                functionTableModel.addAll(portFunctionsMap.get(ports.get(0)));
+                webServiceUI.selectFirstFunction();
+            }
+        } catch (InvocationTargetException e) {
+            webServiceUI.setErrorMessage("Error getting service description: " + e.getCause().getMessage());
+            webServiceUI.setPageComplete(false);
+        } catch (InterruptedException e) {
+            // ignore e.
+        }
 
-	public void resourceNodeSelected(IRepositoryViewObject resourceNode) {
-		currentSetting.setResourceNode(resourceNode);
-	}
+    }
 
-	@Override
-	public void functionSelected(Function function) {
-		webServiceUI.setPageComplete(true);
-	}
+    public IStatus performFinishWithFunction(Function function) {
+        if (function == null) {
+            // select default function to get other port infos from it.
+            function = functionTableModel.getBeansList().get(0);
+        }
+        currentSetting.setFunction(function);
+        if (!currentSetting.isUpdated()) {
+            return Status.OK_STATUS;
+        }
+        IStatus status = nodeAdapter.setNodeSetting(currentSetting);
+        if (status.isOK() && webServiceUI.needPopulateSchema()) {
+            status = populateSchema();
+        }
+        return status;
+    }
+
+    private IStatus populateSchema() {
+        if (currentSetting.isUpdated()) {
+            return SchemaTool.populateSchema(webServiceUI, currentSetting.getDefinition());
+        }
+        return Status.OK_STATUS;
+    }
+
+    public String getInitialWsdlLocation() {
+        return nodeAdapter.getInitialWsdlLocation();
+    }
+
+    public boolean isFunctionRequired() {
+        return nodeAdapter.isServiceOperationRequired();
+    }
+
+    public boolean allowPopulateSchema() {
+        if (WebServiceComponentPlugin.hasRepositoryServices()) {
+            return nodeAdapter.allowPopulateSchema();
+        }
+        return false;
+    }
+
+    public boolean showResourcesButton() {
+        return nodeAdapter.routeResourcesAvailable();
+    }
+
+    public void resourceNodeSelected(IRepositoryViewObject resourceNode) {
+        currentSetting.setResourceNode(resourceNode);
+    }
+
+    @Override
+    public void functionSelected(Function function) {
+        webServiceUI.setPageComplete(true);
+    }
 }
