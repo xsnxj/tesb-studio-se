fd1978e22efafc5649817399e34042a0ef11bacd
Mike Yan <yyan@talend.com>
2016-06-22 20:33:47 +0800

    TESB-17267: Fixed the resource change listener to use repository work unit

--- main/plugins/org.talend.designer.camel.resource/src/org/talend/designer/camel/resource/editors/RouteResoureChangeListener.java
+++ main/plugins/org.talend.designer.camel.resource/src/org/talend/designer/camel/resource/editors/RouteResoureChangeListener.java
@@ -20,7 +20,12 @@ import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IResourceDeltaVisitor;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.swt.widgets.Display;
+import org.talend.commons.exception.LoginException;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.core.runtime.CoreRuntimePlugin;
 import org.talend.designer.camel.resource.editors.input.RouteResourceInput;
+import org.talend.repository.RepositoryWorkUnit;
 
 /**
  * @author xpli
@@ -28,52 +33,82 @@ import org.talend.designer.camel.resource.editors.input.RouteResourceInput;
  */
 public class RouteResoureChangeListener implements IResourceChangeListener {
 
-	private RouteResourceInput editorInput;
-	private IFile editorFile;
-	private String filePath;
+    private static final String SAVING_RESOURCE = "Saving resource"; //$NON-NLS-1$
 
-	public RouteResoureChangeListener(RouteResourceInput editorInput) {
-		this.editorInput = editorInput;
-		this.editorInput.setListener(this);
-		this.editorFile = editorInput.getFile();
-		this.filePath = this.editorFile.getLocation().toPortableString();
-	}
-	@Override
-	public void resourceChanged(IResourceChangeEvent event) {
+    private RouteResourceInput editorInput;
 
-		IResourceDelta delta = event.getDelta();
-		try {
-			if (delta == null || editorFile == null) {
-				return;
-			}
-			delta.accept(new IResourceDeltaVisitor() {
+    private IFile editorFile;
 
-				@Override
-				public boolean visit(IResourceDelta delta) throws CoreException {
-					IResource resource = delta.getResource();
-					if (resource == null) {
-						return false;
-					}
-					if (resource.getType() != IResource.FILE) {
-						IPath location = resource.getLocation();
-						if(location == null || !filePath.startsWith(location.toPortableString())){
-							return false;
-						}
-						return true;
-					}
-					
-					if (resource.equals(editorFile)) {
-						RouteResourceEditor.saveContentsToItem(editorInput);
-						return false;
-					}
-					return false;
-				}
-			}, IResource.FILE);
-		} catch (CoreException e) {
-			e.printStackTrace();
-		}
+    private String filePath;
 
+    public RouteResoureChangeListener(RouteResourceInput editorInput) {
+        this.editorInput = editorInput;
+        this.editorInput.setListener(this);
+        this.editorFile = editorInput.getFile();
+        this.filePath = this.editorFile.getLocation().toPortableString();
+    }
 
-	}
+    /*
+     * yyan:Updated to use work unit
+     * 
+     * @see
+     * org.eclipse.core.resources.IResourceChangeListener#resourceChanged(org.eclipse.core.resources.IResourceChangeEvent
+     * )
+     */
+    @Override
+    public void resourceChanged(IResourceChangeEvent event) {
+
+        IResourceDelta delta = event.getDelta();
+        try {
+            if (delta == null || editorFile == null) {
+                return;
+            }
+            delta.accept(new IResourceDeltaVisitor() {
+
+                @Override
+                public boolean visit(IResourceDelta delta) throws CoreException {
+                    IResource resource = delta.getResource();
+                    if (resource == null) {
+                        return false;
+                    }
+                    if (resource.getType() != IResource.FILE) {
+                        IPath location = resource.getLocation();
+                        if (location == null || !filePath.startsWith(location.toPortableString())) {
+                            return false;
+                        }
+                        return true;
+                    }
+
+                    if (resource.equals(editorFile)) {
+
+                        RepositoryWorkUnit<Object> repositoryWorkUnit = new RepositoryWorkUnit<Object>(SAVING_RESOURCE, this) {
+
+                            @Override
+                            protected void run() throws LoginException, PersistenceException {
+                                RouteResourceEditor.saveContentsToItem(editorInput);
+                            }
+                        };
+                        repositoryWorkUnit.setAvoidUpdateLocks(false);
+                        repositoryWorkUnit.setAvoidUnloadResources(false);
+                        repositoryWorkUnit.setUnloadResourcesAfterRun(false);
+                        Display.getDefault().asyncExec(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                CoreRuntimePlugin.getInstance().getProxyRepositoryFactory()
+                                        .executeRepositoryWorkUnit(repositoryWorkUnit);
+                            }
+                        });
+
+                        return false;
+                    }
+                    return false;
+                }
+            }, IResource.FILE);
+        } catch (CoreException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
