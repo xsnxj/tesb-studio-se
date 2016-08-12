a8a53b46a0300cc30ea9e167d985c6d4df1a62e5
wchen-talend <wchen@talend.com>
2016-08-12 12:49:52 +0800

    Bugfix/6.3/tup 5032 optimize wsdl import (#43)
    * TUP-5033: improve speed of publish from the wsdl
    
    * TUP-5032: improve the wsdl loading
    
    * TUP-5032:[6.1.1] Studio hang issue

--- main/plugins/org.talend.repository.services/src/org/talend/repository/services/ESBService.java
+++ main/plugins/org.talend.repository.services/src/org/talend/repository/services/ESBService.java
@@ -57,6 +57,7 @@ import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.core.repository.model.ProjectRepositoryNode;
 import org.talend.core.repository.model.ProxyRepositoryFactory;
 import org.talend.core.runtime.CoreRuntimePlugin;
+import org.talend.core.utils.IXSDPopulationUtil;
 import org.talend.core.utils.KeywordsValidator;
 import org.talend.designer.core.IDesignerCoreService;
 import org.talend.designer.core.model.components.EParameterName;
@@ -85,6 +86,7 @@ import org.talend.repository.services.model.services.ServicesPackage;
 import org.talend.repository.services.utils.ESBRepositoryNodeType;
 import org.talend.repository.services.utils.OperationRepositoryObject;
 import org.talend.repository.services.utils.PortRepositoryObject;
+import org.talend.repository.services.utils.WSDLPopulationUtil;
 import org.talend.repository.services.utils.WSDLUtils;
 
 /**
@@ -883,7 +885,7 @@ public class ESBService implements IESBService {
             String temp = value;
             while (validJobName(node, temp) != null) {
                 if (j > 'z') {
-                    throw new BusinessException(Messages.ESBService_cannotGenerateItem); //$NON-NLS-1$
+                    throw new BusinessException(Messages.ESBService_cannotGenerateItem);
                 }
                 temp = value + "_" + (j++) + ""; //$NON-NLS-1$ //$NON-NLS-2$
             }
@@ -973,5 +975,15 @@ public class ESBService implements IESBService {
                 arraysList.add(repositoryNodeType);
             }
         }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.core.IESBService#getXSDPopulationUtil()
+     */
+    @Override
+    public IXSDPopulationUtil getXSDPopulationUtil() {
+        return new WSDLPopulationUtil();
     }
 }
