e66e476f8525c025ec17a69c88c904df9431a284
Mike Yan <yyan@talend.com>
2017-03-23 14:04:22 +0800

    TESB-19147: Fixed error thrown in error log after logon studio do expand or right click

--- main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/i18n/Messages.java
+++ main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/i18n/Messages.java
@@ -29,7 +29,7 @@ import org.talend.commons.i18n.MessagesCore;
  */
 public class Messages extends MessagesCore {
 
-    private static final String BUNDLE_NAME = "org.talend.designer.esb.runcontainer.messages"; //$NON-NLS-1$
+    private static final String BUNDLE_NAME = "messages"; //$NON-NLS-1$
 
 	private static final String PLUGIN_ID = "org.talend.camel.designer"; //$NON-NLS-1$
 
