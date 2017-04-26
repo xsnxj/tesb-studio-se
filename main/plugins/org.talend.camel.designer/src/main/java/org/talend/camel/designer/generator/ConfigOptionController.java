46bec790337cc9452e7783a657f59398e57a013c
SunChaoqun <csun@talend.com>
2017-04-26 11:05:29 +0800

    TESB-17306:Connect Talend (ESB) Studio to Nexus

--- main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/generator/ConfigOptionController.java
+++ main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/generator/ConfigOptionController.java
@@ -1,56 +1,26 @@
 package org.talend.camel.designer.generator;
 
 import java.beans.PropertyChangeEvent;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Method;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-import org.apache.maven.artifact.repository.metadata.Metadata;
-import org.apache.maven.artifact.repository.metadata.SnapshotVersion;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.views.properties.tabbed.ITabbedPropertyConstants;
-import org.osgi.framework.Bundle;
-import org.talend.commons.exception.ExceptionHandler;
 import org.talend.commons.ui.runtime.image.ImageProvider;
 import org.talend.commons.ui.swt.tableviewer.TableViewerCreator;
 import org.talend.core.GlobalServiceRegister;
-import org.talend.core.PluginChecker;
-import org.talend.core.model.general.ILibrariesService;
 import org.talend.core.model.general.INexusService;
-import org.talend.core.model.general.ModuleToInstall;
-import org.talend.core.model.process.IContextManager;
 import org.talend.core.model.process.IElementParameter;
-import org.talend.core.model.utils.TalendTextUtils;
-import org.talend.core.runtime.maven.MavenArtifact;
-import org.talend.core.runtime.maven.MavenUrlHelper;
 import org.talend.core.ui.CoreUIPlugin;
 import org.talend.core.ui.properties.tab.IDynamicProperty;
-import org.talend.designer.core.ui.editor.nodes.Node;
 import org.talend.designer.core.ui.editor.properties.controllers.AbstractElementPropertySectionController;
-import org.talend.designer.core.ui.editor.properties.controllers.EmptyContextManager;
-import org.talend.librariesmanager.utils.RemoteModulesHelper;
-import org.talend.repository.ui.wizards.exportjob.util.ExportJobUtil;
-import org.talend.utils.io.FilesUtils;
 
 public class ConfigOptionController extends AbstractElementPropertySectionController {
 
