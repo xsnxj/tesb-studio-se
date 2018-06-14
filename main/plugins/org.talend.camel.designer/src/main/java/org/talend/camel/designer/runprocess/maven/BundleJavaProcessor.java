58f995757769b011c4c793d729a19aca927bbb07
SunChaoqun <csun@talend.com>
2018-06-14 13:36:19 +0800

    TESB-22106:CI for ESB - Data Services and Routes - On Prem - Part 2

--- main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/runprocess/maven/BundleJavaProcessor.java
+++ main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/runprocess/maven/BundleJavaProcessor.java
@@ -12,31 +12,37 @@
 // ============================================================================
 package org.talend.camel.designer.runprocess.maven;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.operation.IRunnableWithProgress;
-import org.talend.camel.core.model.camelProperties.CamelProcessItem;
+import org.talend.camel.designer.build.CreateMavenBundlePom;
 import org.talend.camel.designer.ui.wizards.actions.JavaCamelJobScriptsExportWSAction;
 import org.talend.commons.exception.ExceptionHandler;
 import org.talend.core.GlobalServiceRegister;
 import org.talend.core.model.process.IProcess;
-import org.talend.core.model.properties.ProcessItem;
 import org.talend.core.model.properties.Property;
 import org.talend.core.model.repository.IRepositoryObject;
 import org.talend.core.model.repository.RepositoryObject;
 import org.talend.core.repository.seeker.RepositorySeekerManager;
+import org.talend.core.repository.utils.ItemResourceUtil;
 import org.talend.core.runtime.process.TalendProcessArgumentConstant;
-import org.talend.core.runtime.process.TalendProcessOptionConstants;
+import org.talend.core.runtime.repository.build.AbstractBuildProvider;
+import org.talend.core.runtime.repository.build.BuildExportManager;
+import org.talend.core.runtime.repository.build.IBuildParametes;
+import org.talend.core.runtime.repository.build.IBuildPomCreatorParameters;
 import org.talend.core.runtime.repository.build.IMavenPomCreator;
-import org.talend.core.utils.BitwiseOptionUtils;
 import org.talend.designer.maven.model.TalendMavenConstants;
 import org.talend.designer.maven.tools.AggregatorPomsHelper;
 import org.talend.designer.maven.utils.PomUtil;
 import org.talend.designer.runprocess.ProcessorException;
 import org.talend.designer.runprocess.ProcessorUtilities;
 import org.talend.designer.runprocess.maven.MavenJavaProcessor;
-import org.talend.repository.model.IRepositoryNode;
 import org.talend.repository.model.IRepositoryService;
 
 /**
@@ -133,7 +139,50 @@ public class BundleJavaProcessor extends MavenJavaProcessor {
      */
     @Override
     protected IMavenPomCreator createMavenPomCreator() {
-        return super.createMavenPomCreator();
+        final Property itemProperty = this.getProperty();
+        String buildTypeName = null;
+        // FIXME, better use the arguments directly for run/export/build/..., and remove this flag later.
+        if (ProcessorUtilities.isExportConfig()) {
+            // final Object exportType = itemProperty.getAdditionalProperties().get(MavenConstants.NAME_EXPORT_TYPE);
+            final Object exportType = getArguments().get(TalendProcessArgumentConstant.ARG_BUILD_TYPE);
+            buildTypeName = exportType != null ? exportType.toString() : null;
+        } // else { //if run job, will be null (use Standalone by default)
+
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put(IBuildParametes.ITEM, itemProperty.getItem());
+        parameters.put(IBuildPomCreatorParameters.PROCESSOR, this);
+        parameters.put(IBuildPomCreatorParameters.FILE_POM, getPomFile());
+        parameters.put(IBuildPomCreatorParameters.FILE_ASSEMBLY, getAssemblyFile());
+        parameters.put(IBuildPomCreatorParameters.CP_LINUX, this.unixClasspath);
+        parameters.put(IBuildPomCreatorParameters.CP_WIN, this.windowsClasspath);
+        parameters.put(IBuildPomCreatorParameters.ARGUMENTS_MAP, getArguments());
+        parameters.put(IBuildPomCreatorParameters.OVERWRITE_POM, Boolean.TRUE);
+
+        AbstractBuildProvider foundBuildProvider = BuildExportManager.getInstance().getBuildProvider(buildTypeName, parameters);
+        if (foundBuildProvider != null) {
+            final IMavenPomCreator creator = foundBuildProvider.createPomCreator(parameters);
+            if (creator != null) {
+                return creator;
+            }
+        }
+
+        // normally, won't be here, should return creator in font.
+        CreateMavenBundlePom createTemplatePom = new CreateMavenBundlePom(this, getPomFile());
+
+        createTemplatePom.setUnixClasspath(this.unixClasspath);
+        createTemplatePom.setWindowsClasspath(this.windowsClasspath);
+
+        createTemplatePom.setAssemblyFile(getAssemblyFile());
+
+        IPath itemLocationPath = ItemResourceUtil.getItemLocationPath(this.getProperty());
+        IFolder objectTypeFolder = ItemResourceUtil.getObjectTypeFolder(this.getProperty());
+        if (itemLocationPath != null && objectTypeFolder != null) {
+            IPath itemRelativePath = itemLocationPath.removeLastSegments(1).makeRelativeTo(objectTypeFolder.getLocation());
+            createTemplatePom.setObjectTypeFolder(objectTypeFolder);
+            createTemplatePom.setItemRelativePath(itemRelativePath);
+        }
+
+        return createTemplatePom;
     }
 
     @Override
