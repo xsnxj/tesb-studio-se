7a498630a82ad219e3af410353af7a9d2b8907ff
Stephane Bouchet <sbouchet@talend.com>
2018-09-04 16:38:45 +0200

    feat(TESB-22872):
    - implement try in tester with local running implementation url

--- main/plugins/org.talend.designer.esb.components.rs.provider/src/main/java/org/talend/designer/esb/components/rs/provider/Messages.java
+++ main/plugins/org.talend.designer.esb.components.rs.provider/src/main/java/org/talend/designer/esb/components/rs/provider/Messages.java
@@ -12,32 +12,22 @@
 // ============================================================================
 package org.talend.designer.esb.components.rs.provider;
 
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
 import org.eclipse.osgi.util.NLS;
 
-public class Messages extends NLS{
+public class Messages extends NLS {
 
-	private static final String BUNDLE_NAME = "org.talend.designer.esb.components.rs.provider.messages"; //$NON-NLS-1$
+    private static final String BUNDLE_NAME = "org.talend.designer.esb.components.rs.provider.messages"; //$NON-NLS-1$
 
-	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+    public static String EsbPreferencePage_SL_NAMESPACE;
 
-	public static String EsbPreferencePage_SL_NAMESPACE;
+    public static String EsbPreferencePage_DEFAULT_REST_ENDPOINT_URI;
 
-	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
-	}
+    static {
+        // initialize resource bundle
+        NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+    }
 
-	private Messages() {
-	}
-
-	public static String getString(String key) {
-		try {
-			return RESOURCE_BUNDLE.getString(key);
-		} catch (MissingResourceException e) {
-			return '!' + key + '!';
-		}
-	}
+    private Messages() {
+    }
 
 }
