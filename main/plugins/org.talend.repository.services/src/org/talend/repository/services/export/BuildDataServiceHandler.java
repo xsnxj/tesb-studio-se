b28f3eaf02b74a33d510a4d7c1d5e1a2fbc7f695
Chao MENG <cmeng@talend.com>
2018-10-26 10:16:26 +0800

    fix(TUP-21039): "Apply context to children jobs" does not work for "grand son" job if Studio not restarted (#285)
    * fix(TUP-21039): "Apply context to children jobs" does not work for
    "grand son" job if Studio not restarted
    https://jira.talendforge.org/browse/TUP-21039
    
    * fix(TUP-21039): "Apply context to children jobs" does not work for
    "grand son" job if Studio not restarted
    https://jira.talendforge.org/browse/TUP-21039

--- main/plugins/org.talend.repository.services/src/org/talend/repository/services/export/BuildDataServiceHandler.java
+++ main/plugins/org.talend.repository.services/src/org/talend/repository/services/export/BuildDataServiceHandler.java
@@ -186,6 +186,7 @@ public class BuildDataServiceHandler implements IBuildJobHandler {
      * @see org.talend.repository.ui.wizards.exportjob.handler.BuildJobHandler#build(org.eclipse.core.runtime.
      * IProgressMonitor)
      */
+    @Override
     public void build(IProgressMonitor monitor) throws Exception {
         // final Map<String, Object> argumentsMap = new HashMap<String, Object>();
         // argumentsMap.put(TalendProcessArgumentConstant.ARG_GOAL, TalendMavenConstants.GOAL_PACKAGE);
@@ -197,9 +198,12 @@ public class BuildDataServiceHandler implements IBuildJobHandler {
         argumentsMap.put(TalendProcessArgumentConstant.ARG_GOAL, TalendMavenConstants.GOAL_PACKAGE);
         argumentsMap.put(TalendProcessArgumentConstant.ARG_PROGRAM_ARGUMENTS, getProgramArgs());
 
-        talendProcessJavaProject.buildModules(monitor, null, argumentsMap);
-
-        BuildCacheManager.getInstance().performBuildSuccess();
+        try {
+            talendProcessJavaProject.buildModules(monitor, null, argumentsMap);
+            BuildCacheManager.getInstance().performBuildSuccess();
+        } finally {
+            BuildCacheManager.getInstance().clearAllCaches();
+        }
     }
 
     protected String getProgramArgs() {
@@ -317,6 +321,7 @@ public class BuildDataServiceHandler implements IBuildJobHandler {
      */
     @Override
     public IProcessor generateJobFiles(IProgressMonitor monitor) throws Exception {
+        BuildCacheManager.getInstance().clearAllCaches();
 
         // TESB-22307
         if (GlobalServiceRegister.getDefault().isServiceRegistered(IESBService.class)) {
